2d4893a8213f1eb3816a3c26c3ee5a9c584a897f
unknown <Nived.Sivadas@samraksh.com>
2012-01-20 12:45:31 -0500

    Committing the changes to bootstrap like increasing speed to 48Mhz

--- MicroFrameworkPK_v4_0/Solutions/STM32F10x/DeviceCode/drivers/bootstrap/STM32F10x_bootstrap.cpp
+++ MicroFrameworkPK_v4_0/Solutions/STM32F10x/DeviceCode/drivers/bootstrap/STM32F10x_bootstrap.cpp
@@ -55,18 +55,24 @@ static void SetSysClockTo72(void)
     /* Enable Prefetch Buffer */
     FLASH->ACR |= FLASH_ACR_PRFTBE;
 
-    /* Flash 2 wait state */
+    /* Flash wait state depends on frequency of system clock*/
+    //Mukundan modified code
     FLASH->ACR &= (uint32_t)((uint32_t)~FLASH_ACR_LATENCY);
-    FLASH->ACR |= (uint32_t)FLASH_ACR_LATENCY_2;
+   /* if (SYSTEM_CLOCK_HZ <= 24000000)
+    	FLASH->ACR |= (uint32_t)FLASH_ACR_LATENCY_1;
+    else if (SYSTEM_CLOCK_HZ <= 48000000)
+    	FLASH->ACR |= (uint32_t)FLASH_ACR_LATENCY_1;
+    else*/
 
+    FLASH->ACR |= (uint32_t)FLASH_ACR_LATENCY_1;
 
-    /* HCLK = SYSCLK */
+    // HCLK = SYSCLK
     RCC->CFGR |= (uint32_t)RCC_CFGR_HPRE_DIV1;
 
-    /* PCLK2 = HCLK */
+    // PCLK2 = HCLK
     RCC->CFGR |= (uint32_t)RCC_CFGR_PPRE2_DIV1;
 
-    /* PCLK1 = HCLK */
+    // PCLK1 = HCLK
     RCC->CFGR |= (uint32_t)RCC_CFGR_PPRE1_DIV2;
 
 #ifdef STM32F10X_CL
@@ -165,28 +171,34 @@ void LowLevelInit (void)
 
   /* Configure the System clock frequency, HCLK, PCLK2 and PCLK1 prescalers */
   /* Configure the Flash Latency cycles and enable prefetch buffer */
-  SetSysClockTo72();
+  //SetSysClockTo72();
 
 }
 
+// RCC_Configuration : Controls the speed at which the System Clock, ADCClock, AHB, APB1 and APB2 run
+//  System Clock runs off the HSI at 48 Mhz
+//  ADC Clock runs at 12 Mhz
+//  HCLK, AHB run at 48 Mhz
+//  APB2 runs at 24 Mhz
+//  APB1 runs at 24 Mhz
 static void RCC_Configuration(void)
 {
 
   RCC_DeInit();
 
-  RCC_HSEConfig(RCC_HSE_ON);
+  RCC_HSEConfig(RCC_HSE_OFF);
 
-  HSEStartUpStatus = RCC_WaitForHSEStartUp();
+  //HSEStartUpStatus = RCC_WaitForHSEStartUp();
 
 
-  if(HSEStartUpStatus == SUCCESS)
-  {
+  //if(HSEStartUpStatus == SUCCESS)
+  //{
 
     //hal_printf("Enable Prefetch Buffer/n");
     FLASH_PrefetchBufferCmd(FLASH_PrefetchBuffer_Enable);
 
     //hal_printf("Flash 2 wait state/n");
-    FLASH_SetLatency(FLASH_Latency_2);
+    FLASH_SetLatency(FLASH_Latency_1);
 
     //hal_printf("/* HCLK = SYSCLK *//n");
     RCC_HCLKConfig(RCC_SYSCLK_Div1);
@@ -197,14 +209,14 @@ static void RCC_Configuration(void)
 
     //hal_printf("/* PCLK1 = HCLK/2 *//n");
     //Kartik : Changed it to RCC_HCLK_Div1 from RCC_HCLK_Div2
-    RCC_PCLK1Config(RCC_HCLK_Div1);
+    RCC_PCLK1Config(RCC_HCLK_Div2);
 
     //hal_printf("/* ADCCLK = PCLK2/4 *//n");
     RCC_ADCCLKConfig(RCC_PCLK2_Div4);
 
     //hal_printf("/* PLLCLK = 8MHz * 7 = 56 MHz *//n");
     //RCC_PLLConfig(RCC_PLLSource_HSE_Div1, RCC_PLLMul_7);
-	RCC_PLLConfig(RCC_PLLSource_HSE_Div1, RCC_PLLMul_9);
+	RCC_PLLConfig(RCC_PLLSource_HSI_Div2, RCC_PLLMul_12);
 
     //hal_printf("/* Enable PLL *//n");
     RCC_PLLCmd(ENABLE);
@@ -218,17 +230,17 @@ static void RCC_Configuration(void)
     //hal_printf("/* Select PLL as system clock source *//n");
     //RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK);
 	// Kartik : Change made to system clock from 72 to 8 Mhz; It is now powered of the HSI Oscillator
-	RCC_SYSCLKConfig(RCC_SYSCLKSource_HSI);
-	//RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK);
+	//RCC_SYSCLKConfig(RCC_SYSCLKSource_HSI);
+	RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK);
 
     //hal_printf("/* Wait till PLL is used as system clock source *//n");
-    while(RCC_GetSYSCLKSource() != 0x00)
-	//while(RCC_GetSYSCLKSource() != 0x08)
+    //while(RCC_GetSYSCLKSource() != 0x00) //if sysclk is HSI
+	while(RCC_GetSYSCLKSource() != 0x08)	//if sysclk is PLL
     {
     }
 
 
-  }
+  //}
 
 /* Enable peripheral clocks --------------------------------------------------*/
 
