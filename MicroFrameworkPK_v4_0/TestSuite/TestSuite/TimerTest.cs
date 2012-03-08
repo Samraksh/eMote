c2e18a5e0e231fd18625c0080396051ad333a150
unknown <Nived.Sivadas@samraksh.com>
2012-03-08 11:31:38 -0500

    Checking In TimerTest

--- MicroFrameworkPK_v4_0/TestSuite/TestSuite/TimerTest.cs
+++ MicroFrameworkPK_v4_0/TestSuite/TestSuite/TimerTest.cs
@@ -17,8 +17,7 @@ namespace TestSuite
 {
     public class TimerTest
     {
-		//Debug.Print("Executing now.. ");
-        //private OutputPort testPort = new OutputPort(Pins.GPIO_PORT_PIN_0, true);
+		
 		public static bool state = false;
 		private static OutputPort testPort = new OutputPort(Pins.GPIO_PORT_PIN_57, true);		
         Log log = new Log();
@@ -26,53 +25,25 @@ namespace TestSuite
         public void Run()
         {
 			
-			
             try
             {
 			
 				Timer tmr = new Timer(new TimerCallback(Callback), null, 0, 5);								
 				while (true)
                 {
-					//Thread.Sleep(10000);
-                   //DateTime start = DateTime.Now;
-                    //log.log("Time :" + start + "\n");
-                    // Will enable harness or external hardware to check accurate times
-					testPort.Write(true);
-                    //Thread.Sleep(1000);
-					testPort.Write(false);
-                //   log.log("Test Complete \n");
+					Thread.Sleep(100);
                 }				
             }
             catch (Exception)
             {
-                // Will enable harness or external hardware to check accurate times
                 
-                log.log("Unexpected Exception During Timer Test\n");
             }
         }
         private void Callback(Object state)
         {
             testPort.Write(true);						
 			testPort.Write(false);
-            //log.log("Time :\n" + DateTime.Now);
-            //log.log("Call Back Happening\n");
-			//Toggle();
         }	
-        
-		private void Toggle()
-		{			
-			if(state == true)
-			{
-				testPort.Write(false);
-				state = false;
-			}
-			else
-			{
-				testPort.Write(true);
-				state = true;
-			}			
-		}
-
 
     }
 }
