ffdfbfba7de870d4a4509f9389434e3a57796f42
unknown <Nived.Sivadas@samraksh.com>
2012-03-08 11:36:57 -0500

    Checking in the spi demo test

--- MicroFrameworkPK_v4_0/TestSuite/TestSuite/SPITest.cs
+++ MicroFrameworkPK_v4_0/TestSuite/TestSuite/SPITest.cs
@@ -13,26 +13,40 @@ namespace TestSuite
             SPI.Configuration config = new SPI.Configuration(Pins.GPIO_PORT_PIN_1, false, 0, 0, false, true, 16, SPI.SPI_module.SPI1);
 
             SPI testSPI = new SPI(config);
-			
-			int i = 0;
 
             byte[] tx_data = new byte[1];		
             byte[] rx_data = new byte[1];
 
-            tx_data[0] = (byte)192;			
+            //tx_data[0] = (byte)192;			
 
-            //while (true)
-            //{                
+            while (true)
+            { 
+				tx_data[0] = (byte) 1;
                 testSPI.WriteRead(tx_data, rx_data);
-				while(i <400)
-				{ i++; }
-				tx_data[0] = (byte)157;
-				testSPI.WriteRead(tx_data, rx_data);
-                //tx_data[0] = (byte) (tx_data[0]+ 1);				
-                Debug.Print("" + (int)(rx_data[0]));
-                //Thread.Sleep(2);
-            //}
-
+				Thread.Sleep(2000);
+				tx_data[0] = (byte) 2;
+                testSPI.WriteRead(tx_data, rx_data);
+				Thread.Sleep(2000);
+				tx_data[0] = (byte) 3;
+                testSPI.WriteRead(tx_data, rx_data);
+				Thread.Sleep(2000);
+				tx_data[0] = (byte) 4;
+                testSPI.WriteRead(tx_data, rx_data);
+				Thread.Sleep(2000);
+				tx_data[0] = (byte) 5;
+                testSPI.WriteRead(tx_data, rx_data);
+				Thread.Sleep(2000);
+				tx_data[0] = (byte) 6;
+                testSPI.WriteRead(tx_data, rx_data);
+				Thread.Sleep(2000);
+				tx_data[0] = (byte) 7;
+                testSPI.WriteRead(tx_data, rx_data);
+				Thread.Sleep(2000);
+				tx_data[0] = (byte) 8;
+                testSPI.WriteRead(tx_data, rx_data);
+				Thread.Sleep(2000);
+				
+            }
 
         }
 
