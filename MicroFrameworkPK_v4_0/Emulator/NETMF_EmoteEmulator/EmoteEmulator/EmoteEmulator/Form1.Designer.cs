cb6f4cb90cca3f0fa1206de876925c2354a35c86
Mukundan Sridharan <mukundan.sridharn@samraksh.com>
2012-05-07 14:16:10 -0400

    Mukundan: Emote Emulator version 0: Gpio, leds, timers, serial port. Physical Model, built along with emulator. A open-loop car is the physical model.

---
+++ MicroFrameworkPK_v4_0/Emulator/NETMF_EmoteEmulator/EmoteEmulator/EmoteEmulator/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace EmoteEmulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.statusBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.led1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.led2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.led3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.inputEmulation = new System.Windows.Forms.Button();
+            this.button_carGUI = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(14, 238);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(125, 239);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // statusBox
+            // 
+            this.statusBox.Location = new System.Drawing.Point(16, 82);
+            this.statusBox.Multiline = true;
+            this.statusBox.Name = "statusBox";
+            this.statusBox.Size = new System.Drawing.Size(302, 72);
+            this.statusBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(110, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Emulator Status";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(44, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(256, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Samraksh Emote Emulator";
+            // 
+            // led1
+            // 
+            this.led1.BackColor = System.Drawing.Color.MistyRose;
+            this.led1.Location = new System.Drawing.Point(39, 208);
+            this.led1.Name = "led1";
+            this.led1.Size = new System.Drawing.Size(23, 20);
+            this.led1.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(32, 189);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "LED 1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(144, 189);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 16);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "LED 2";
+            // 
+            // led2
+            // 
+            this.led2.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.led2.Location = new System.Drawing.Point(152, 208);
+            this.led2.Name = "led2";
+            this.led2.Size = new System.Drawing.Size(23, 20);
+            this.led2.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(256, 189);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 16);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "LED 3";
+            // 
+            // led3
+            // 
+            this.led3.BackColor = System.Drawing.Color.Honeydew;
+            this.led3.Location = new System.Drawing.Point(265, 208);
+            this.led3.Name = "led3";
+            this.led3.Size = new System.Drawing.Size(23, 20);
+            this.led3.TabIndex = 11;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(240, 240);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // inputEmulation
+            // 
+            this.inputEmulation.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputEmulation.Location = new System.Drawing.Point(16, 274);
+            this.inputEmulation.Name = "inputEmulation";
+            this.inputEmulation.Size = new System.Drawing.Size(145, 23);
+            this.inputEmulation.TabIndex = 14;
+            this.inputEmulation.Text = "Start Input Emulation";
+            this.inputEmulation.UseVisualStyleBackColor = true;
+            this.inputEmulation.Click += new System.EventHandler(this.inputEmulation_Click);
+            // 
+            // button_carGUI
+            // 
+            this.button_carGUI.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_carGUI.Location = new System.Drawing.Point(173, 274);
+            this.button_carGUI.Name = "button_carGUI";
+            this.button_carGUI.Size = new System.Drawing.Size(145, 23);
+            this.button_carGUI.TabIndex = 15;
+            this.button_carGUI.Text = "Display CarGUI";
+            this.button_carGUI.UseVisualStyleBackColor = true;
+            this.button_carGUI.Click += new System.EventHandler(this.button_carGUI_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 309);
+            this.Controls.Add(this.button_carGUI);
+            this.Controls.Add(this.inputEmulation);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.led3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.led2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.led1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.statusBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox statusBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox led1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox led2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox led3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button inputEmulation;
+        private System.Windows.Forms.Button button_carGUI;
+    }
+}
+
---
+++ MicroFrameworkPK_v4_0/Emulator/NETMF_EmoteEmulator/EmoteEmulator/OpenLoopCarEmulation/App.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using Microsoft.SPOT.Messaging;
+
+using System.IO.Ports;
+
+namespace OpenLoopCarEmulation
+{
+    public class CarEmulationTest
+    {
+     
+        InterruptPort _button1_InterruptPort, _button2_InterruptPort, _button3_InterruptPort;
+        static OutputPort _led1_port, _led2_port, _led3_port;
+        static bool button1_state, button2_state, button3_state;
+        Timer mainTimer;
+        SerialPort mySerialPort;
+        CarPMInput carInput;
+        public const byte STOP = 255;
+
+        uint X, Y = 0;
+
+        public CarEmulationTest()
+        {
+           /* _button1_InterruptPort = new InterruptPort((Cpu.Pin)9, false, Port.ResistorMode.PullDown, Port.InterruptMode.InterruptEdgeBoth);
+            _button2_InterruptPort = new InterruptPort((Cpu.Pin)10, false, Port.ResistorMode.PullDown, Port.InterruptMode.InterruptEdgeBoth);
+            _button3_InterruptPort = new InterruptPort((Cpu.Pin)22, false, Port.ResistorMode.PullDown, Port.InterruptMode.InterruptEdgeBoth);
+
+            _button1_InterruptPort.OnInterrupt += new NativeEventHandler(button1_OnInterrupt);
+            _button2_InterruptPort.OnInterrupt += new NativeEventHandler(button2_OnInterrupt);
+            _button3_InterruptPort.OnInterrupt += new NativeEventHandler(button3_OnInterrupt);
+            
+            //_led1_port = new OutputPort((Cpu.Pin)21, false);
+            //_led2_port = new OutputPort((Cpu.Pin)4, false);
+            //_led3_port = new OutputPort((Cpu.Pin)38, false);
+          
+            _led1_port = new OutputPort((Cpu.Pin)1, false);
+            _led2_port = new OutputPort((Cpu.Pin)2, false);
+            _led3_port = new OutputPort((Cpu.Pin)3, false);*/
+           
+            
+            //mySerialPort = new SerialPort("COM2", 115200, Parity.None);
+            try
+            {
+                mySerialPort = new SerialPort("COM1");
+            }
+            catch (Exception e)
+            {
+                Debug.Print(e.ToString());
+            }
+
+           // mainTimer = new Timer(new TimerCallback(readModelParameters), null, 0, 1000);
+            carInput = new CarPMInput();
+
+        }
+
+        void readModelOutput(object state)
+        {
+            //X += 4;
+            //Y += 2;
+            //(intToBytes(X)).CopyTo(buffer, 0);
+            //(intToBytes(Y)).CopyTo(buffer, 4);
+
+            //mySerialPort.Handshake = Handshake.None;
+            //mySerialPort.Parity = Parity.None;
+            //mySerialPort.BaudRate = 9600;
+            //mySerialPort.StopBits = StopBits.One;
+            //mySerialPort.DataBits = 8;
+
+            //Debug.Print("PortString: " +mySerialPort.ToString());
+            //Debug.Print("PortHash: " +(mySerialPort.GetHashCode()).ToString());
+            if (!mySerialPort.IsOpen)
+            {
+                mySerialPort.Open();
+            }
+            
+
+        }
+
+        void sendModelInput()
+        {
+            byte[] buffer = new byte[4];
+
+            ShortToBytes(carInput.SteeringAngle, buffer);
+            buffer[2] = (byte)carInput.Accelaration;
+            buffer[3] = carInput.State;
+            if (!mySerialPort.IsOpen)
+            {
+                mySerialPort.Open();
+            }
+       
+            int n = mySerialPort.Write(buffer, 0, buffer.Length);
+
+            if (n <= 0)
+            {
+                Debug.Print("Failure: " + n.ToString());
+            }
+            else
+            {
+                Debug.Print("Success, sent: " + n.ToString());
+            }
+        }
+
+        byte[] intToBytes(uint x)
+        {
+            byte[] bytes = new byte[4];
+            bytes[3] = (byte)(x >> 24);
+            bytes[2] = (byte)(x >> 16);
+            bytes[1] = (byte)(x >> 8);
+            bytes[0] = (byte)x;
+            return bytes;
+        }
+        void ShortToBytes(short x, byte[] ret)
+        {
+            //byte[] bytes = new byte[2];
+            ret[1] = (byte)(x >> 8);
+            ret[0] = (byte)x;
+            //return bytes;
+        }
+
+        static void button1_OnInterrupt(uint data1, uint data2, DateTime time)
+        {
+            //if (button1_state) button1_state = false; else button1_state = true;
+
+            //_led1_port.Write(button1_state);
+            Debug.Print("Button 1" + ((button1_state) ? "Down" : "Up"));
+        }
+
+        static void button2_OnInterrupt(uint data1, uint data2, DateTime time)
+        {
+           // if (button2_state) button2_state = false; else button2_state = true;
+            //_led2_port.Write(button2_state);
+            Debug.Print("Button 2: " + "Go Left");
+        }
+
+        static void button3_OnInterrupt(uint data1, uint data2, DateTime time)
+        {
+            //if (button3_state) button3_state = false; else button3_state = true;
+            //_led3_port.Write(button3_state);
+            Debug.Print("Button 3: " + "No Action");
+        }
+
+        void Turn(short degrees)
+        {
+            carInput.SteeringAngle = degrees;
+            carInput.State = 0;
+            carInput.Accelaration = 0;
+            sendModelInput();
+        }
+        void Stop()
+        {
+            carInput.State = STOP;
+            sendModelInput();
+        }
+
+        void Drive_A_Eight(){
+
+            Thread.Sleep(5000); //Drive Straight for 5s
+            
+            Turn(70);
+            Thread.Sleep(3000); //Slight Left for 3s
+            Turn(-70);
+
+            Thread.Sleep(5000); //Drive Straight for 5s
+
+            Turn(-90); //Turn right (almost u-turn) 13s
+            Thread.Sleep(13000);
+            Turn(90);
+
+            Thread.Sleep(10000); //Drive Straight for 10s
+
+            Turn(90); //Turn left for 5s
+            Thread.Sleep(5000); 
+            Turn(-90);
+
+            Thread.Sleep(5000); //Drive Straight for 5s
+
+            Turn(-90);//Turn right for 10s
+            Thread.Sleep(10000);
+            Turn(90);
+
+            Thread.Sleep(7000);//Drive Straight for 10s
+            Stop();
+
+
+        }
+       
+        
+        public static void Main()
+        {
+            
+            CarEmulationTest c = new CarEmulationTest();
+            //Debug.Print("Starting a 8 Test");
+            c.Drive_A_Eight();
+            //Debug.Print("Done with 8 Test");
+                        
+            //Sleep Indefinitely
+            Thread.Sleep(Timeout.Infinite);
+        }
+        
+    }
+}
