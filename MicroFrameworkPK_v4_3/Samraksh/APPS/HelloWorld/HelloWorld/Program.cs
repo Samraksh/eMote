d117276532a0a1e463976abd61b54887a0a8e81f
Nived Sivadas <Nived.Sivadas@samraksh.com>
2012-12-02 15:53:09 -0500

    Checking in HelloWorld over usart

---
+++ MicroFrameworkPK_v4_3/Samraksh/APPS/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.SPOT;
+using System.Threading;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            while (true)
+            {
+                Debug.Print("Hello World");
+                Thread.Sleep(1000);
+            }
+        }
+
+    }
+}
