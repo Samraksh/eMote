53af7c9988a2b9bdef87a465a71d116c97afeb77
Nived Sivadas <Nived.Sivadas@samraksh.com>
2013-01-14 13:21:44 -0500

    Checking in modified usart driver

---
+++ MicroFrameworkPK_v4_3/Framework/Subset_of_CPP/SpotVisualCpp/SpotVisualCpp/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.SPOT;
+
+namespace System.Runtime.InteropServices
+{
+	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, Inherited = false)]
+    public sealed class MarshalAsAttribute : Attribute
+    {
+
+        
+
+        public MarshalAsAttribute(short unmanagedType)
+        {
+        }
+
+
+    }
+}
