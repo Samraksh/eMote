b4aa75ee8b5bef9686a4222c86873467ed690686
Nived Sivadas <Nived.Sivadas@samraksh.com>
2013-04-16 09:51:02 -0400

    Adding preemptive app to the list of apps

---
+++ MicroFrameworkPK_v4_3/Samraksh/APPS/RealTimeExample/RealTimeExample/RealTimeExample.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using Samraksh.SPOT.RealTime;
+using System.Threading;
+
+namespace RealTimeExample
+{
+    public class RealTimeExample
+    {
+        Samraksh.SPOT.RealTime.Timer RT_Timer;
+        static string outstr = "RealTime Handler";
+        static bool RT_Timer_State;
+        NativeEventHandler RT_EventHandler = new NativeEventHandler(RT_TimerCallback);
+        static OutputPort Output = new OutputPort((Cpu.Pin)30, true);
+        static OutputPort Error = new OutputPort((Cpu.Pin)31, true);
+
+        RealTimeExample()
+        {
+            try
+            {
+                RT_Timer = new Samraksh.SPOT.RealTime.Timer("RealTimeInteropTimer", 10000, 0);
+                RT_Timer.OnInterrupt += RT_EventHandler;
+            }
+            catch (Exception)
+            {
+                Debug.Print("Error initializing Realtime timer");
+                while (true)
+                {
+                    Error.Write(true);
+                    Thread.Sleep(100);
+                    Error.Write(false);
+                }
+            }
+        }
+
+        private static void RT_TimerCallback(uint data1, uint data2, DateTime time)
+        {
+            //Debug.Print(outstr);
+            RT_Timer_State = !RT_Timer_State;
+            Output.Write(RT_Timer_State);
+            RT_Timer_State = !RT_Timer_State;
+            Output.Write(RT_Timer_State);
+        } 
+
+        public static void Main()
+        {
+            Debug.Print("Starting the RT timer");
+            RealTimeExample rt = new RealTimeExample();
+        }
+
+    }
+}
---
+++ MicroFrameworkPK_v4_3/Samraksh/APPS/ShowPremptiveTimer/ShowPremptiveTimer/ShowPremptiveTimer/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+
+
+namespace ShowPremptiveTimer
+{
+    public class Program
+    {
+        static string msgString = "RealTime.Timer";
+        public static void Main()
+        {
+            Samraksh.SPOT.RealTime.Timer rt_timer;
+            rt_timer = new Samraksh.SPOT.RealTime.Timer("RealTimeInteropTimer", 500,0);
+            NativeEventHandler RT_EventHandler = new NativeEventHandler(RT_TimerCallback);
+            rt_timer.OnInterrupt += RT_EventHandler;
+
+            Thread thread1 = new Thread(XActivity);
+            Thread thread2 = new Thread(YActivity);
+
+            thread1.Start();
+            thread2.Start(); 
+
+        }
+        static void RT_TimerCallback (uint data1, uint data2, DateTime time)
+        {
+            Debug.Print(msgString);
+        }
+
+
+         static void XActivity() 
+        { 
+            int j; 
+            while (true) 
+            { 
+                Debug.Print("X"); 
+                for (int i = 0; i < 50; i++) 
+                { 
+                    j = i * 3; 
+                } 
+            } 
+        }
+         static void YActivity()
+         {
+             int j;
+             while (true)
+             {
+                 Debug.Print(" Y");
+                 for (int i = 0; i < 50; i++)
+                 {
+                     j = i * 3;
+                 }
+             }
+         }
+    }
+}
