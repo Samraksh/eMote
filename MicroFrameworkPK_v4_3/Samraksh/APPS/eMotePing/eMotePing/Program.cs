1deb722993827b7ce38d6ca55d88be158e67a307
ChrisAtSamraksh <chris.morse@samraksh.com>
2013-05-14 15:34:52 -0400

    eMotePing update
    emotePing updated to use latest CSMA code

--- MicroFrameworkPK_v4_3/Samraksh/APPS/eMotePing/eMotePing/Program.cs
+++ MicroFrameworkPK_v4_3/Samraksh/APPS/eMotePing/eMotePing/Program.cs
@@ -18,7 +18,7 @@ namespace Samraksh.SPOT.Net.Mac.Ping
     }
 
 
-    public class PingMsg 
+    public class PingMsg
     {
         public bool Response;
         public ushort MsgID;
@@ -28,25 +28,26 @@ namespace Samraksh.SPOT.Net.Mac.Ping
         public PingMsg()
         {
         }
-        public static  int Size(){
+        public static int Size()
+        {
             return 7;
         }
         public PingMsg(byte[] rcv_msg, ushort size)
         {
-            Response = rcv_msg[0]==0 ? false: true ;
-            MsgID = (UInt16) (rcv_msg[1] << 8);
+            Response = rcv_msg[0] == 0 ? false : true;
+            MsgID = (UInt16)(rcv_msg[1] << 8);
             MsgID += (UInt16)rcv_msg[2];
             Src = (UInt16)(rcv_msg[3] << 8);
             Src += (UInt16)rcv_msg[4];
             dummySrc = (UInt16)(0xefef);
-            
+
         }
 
         public byte[] ToBytes()
         {
             byte[] b_msg = new byte[7];
-            b_msg[0] = Response ? (byte)1: (byte)0;
-            b_msg[1] = (byte) ((MsgID >> 8) & 0xFF);
+            b_msg[0] = Response ? (byte)1 : (byte)0;
+            b_msg[1] = (byte)((MsgID >> 8) & 0xFF);
             b_msg[2] = (byte)(MsgID & 0xFF);
             b_msg[3] = (byte)((Src >> 8) & 0xFF);
             b_msg[4] = (byte)(Src & 0xFF);
@@ -57,7 +58,7 @@ namespace Samraksh.SPOT.Net.Mac.Ping
     }
 
     public class Program
-    {  
+    {
         UInt16 myAddress;
         UInt16 mySeqNo = 0;
         Timer sendTimer;
@@ -71,37 +72,38 @@ namespace Samraksh.SPOT.Net.Mac.Ping
         static OutputPort SendPort = new OutputPort((Cpu.Pin)30, true);
         static OutputPort ReceivePort = new OutputPort((Cpu.Pin)31, true);
 
-        static Mac.CSMA myCSMA = new CSMA();
+        static Mac.CSMA myCSMA;
+        ReceiveCallBack myReceiveCB;
+        NeighbourhoodChangeCallBack myNeighborCB;
 
         Mac.MacConfiguration macConfig = new MacConfiguration();
-        ReceiveCallBack myReceive;
+
 
         void Initialize()
         {
-            
+
             Debug.Print("Initializing:  EmotePingwLCD");
             Thread.Sleep(1000);
             lcd = new EmoteLCD();
             lcd.Initialize();
             lcd.Write(LCD.CHAR_I, LCD.CHAR_N, LCD.CHAR_I, LCD.CHAR_7);
-                    
-            macConfig.CCA = true;
-            macConfig.BufferSize = 8;
-            macConfig.NumberOfRetries = 0;
-            //macConfig.RadioID = (byte) myRadioID;
-            macConfig.RadioID = (byte)1;
+
+            macConfig.NeighbourLivelinesDelay = 180;
             macConfig.CCASenseTime = 140; //Carries sensing time in micro seconds
 
-            myReceive = HandleMessage; //Assign the delegate to a function
-            
-            Debug.Print("Initializing:  CSMA...");
-            try{
-                  myCSMA.Initialize(macConfig, myReceive);
+            Debug.Print("Configuring:  CSMA...");
+            try
+            {
+                myReceiveCB = Receive;
+                myNeighborCB = NeighborChange;
+                CSMA.Configure(macConfig, myReceiveCB, myNeighborCB);
+                myCSMA = CSMA.Instance;                
             }
             catch (Exception e)
             {
                 Debug.Print(e.ToString());
             }
+
             Debug.Print("CSMA Init done.");
             myAddress = myCSMA.GetAddress();
             Debug.Print("My default address is :  " + myAddress.ToString());
@@ -109,8 +111,8 @@ namespace Samraksh.SPOT.Net.Mac.Ping
             /*myCSMA.SetAddress(52);
             myAddress = myCSMA.GetAddress();
             Debug.Print("My New address is :  " + myAddress.ToString());
-             */ 
-           
+             */
+
         }
         void Start()
         {
@@ -118,12 +120,12 @@ namespace Samraksh.SPOT.Net.Mac.Ping
             sendTimer = new Timer(new TimerCallback(sendTimerCallback), null, 0, 100);
             Debug.Print("Timer init done.");
         }
-        
+
         void sendTimerCallback(Object o)
         {
             //mySeqNo++;
-            if((mySeqNo % 50) == 0)
-            Debug.Print("Sending broadcast ping msg:  " + mySeqNo.ToString());
+            if ((mySeqNo % 50) == 0)
+                Debug.Print("Sending broadcast ping msg:  " + mySeqNo.ToString());
             try
             {
                 Send_Ping(sendMsg);
@@ -134,6 +136,20 @@ namespace Samraksh.SPOT.Net.Mac.Ping
             }
 
         }
+
+        void NeighborChange(UInt16 noOfNeigbors)
+        {
+        }
+
+        void Receive(UInt16 noOfPackets)
+        {
+            Message rcvMsg = myCSMA.GetNextPacket();
+            byte[] rcvPayload = rcvMsg.GetMessage();
+            HandleMessage(rcvPayload, (UInt16)rcvPayload.Length, rcvMsg.Src, rcvMsg.Unicast, rcvMsg.RSSI, rcvMsg.LQI);
+
+        }
+
+
 
         void HandleMessage(byte[] msg, UInt16 size, UInt16 src, bool unicast, byte rssi, byte lqi)
         {
@@ -147,9 +163,10 @@ namespace Samraksh.SPOT.Net.Mac.Ping
                 {
                     Debug.Print("Got a broadcast message from src: " + src.ToString() + ", size: " + size.ToString() + ", rssi: " + rssi.ToString() + ", lqi: " + lqi.ToString());
                 }
-                if (size == PingMsg.Size())
+                //if (size == PingMsg.Size())
+                if (size == 128)
                 {
-                  
+
                     //Debug.Print("MSG: " + msg[0].ToString() + " " + msg[1].ToString() + " " + msg[2].ToString() + " " + msg[3].ToString() + " " + msg[4].ToString() + " " + msg[5].ToString());
                     PingMsg rcvMsg = new PingMsg(msg, size);
 
@@ -175,7 +192,7 @@ namespace Samraksh.SPOT.Net.Mac.Ping
                 Debug.Print(e.ToString());
             }
         }
-   
+
         void Send_Pong(PingMsg ping)
         {
             UInt16 sender = ping.Src;
@@ -185,7 +202,7 @@ namespace Samraksh.SPOT.Net.Mac.Ping
 
             byte[] msg = ping.ToBytes();
             myCSMA.Send(sender, msg, 0, (ushort)msg.Length);
-            
+
         }
 
         void Send_Ping(PingMsg ping)
@@ -193,9 +210,9 @@ namespace Samraksh.SPOT.Net.Mac.Ping
             //UInt16 sender = ping.Src;
             //Debug.GC(true);
             ping.Response = false;
-            ping.MsgID=mySeqNo++;
+            ping.MsgID = mySeqNo++;
             ping.Src = myAddress;
-            
+
             SendPort.Write(true);
             SendPort.Write(false);
 
@@ -203,12 +220,12 @@ namespace Samraksh.SPOT.Net.Mac.Ping
             myCSMA.Send((UInt16)Mac.Addresses.BROADCAST, msg, 0, (ushort)msg.Length);
             int char0 = (mySeqNo % 10) + (int)LCD.CHAR_0;
             lcd.Write(LCD.CHAR_S, LCD.CHAR_S, LCD.CHAR_S, (LCD)char0);
-         
+
         }
 
         public static void Main()
         {
-            Debug.Print("Changing app");
+            //while (true) { Debug.Print("Changing app"); }
             Program p = new Program();
             p.Initialize();
             p.Start();
