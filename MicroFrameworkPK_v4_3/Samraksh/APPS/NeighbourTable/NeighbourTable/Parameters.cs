b6515bb62b2a3d5ad75766cc1e124432ba0009e6
ChrisAtSamraksh <chris.morse@samraksh.com>
2013-05-20 12:39:53 -0400

    NeighborTable checkin

---
+++ MicroFrameworkPK_v4_3/Samraksh/APPS/NeighbourTable/NeighbourTable/Parameters.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ParameterClass
+{
+    class Parameters
+    {
+	// required TestRig parameters
+	public bool useLogic = false;
+	public double sampleTimeMs = 4000;
+	public double sampleFrequency = 4000000;
+	public bool useExecutable = false;
+	public string executableName = "example.exe";
+	public int executableTimeoutMs = 9000;
+	public bool useMatlabAnalysis = false;
+	public string matlabScriptName = "analyze.m";
+	public bool usePowershellAnalysis = false;
+	public string powershellName = "analyze.ps1";
+	// Do not change text format above this point
+    }
+}
