74d0fb3718c0ccb35a1d8e95a0645ecb7d117c95
ChrisAtSamraksh <chris.morse@samraksh.com>
2014-02-10 13:50:51 -0500

    Samraksh.SPOT.Hardware.Adapt stubs checking
    Here is the initial stubs checkin at
    DotNet-MF\MicroFrameworkPK_v4_3\DeviceCode\Targets\Native\Krait\DeviceCode

---
+++ MicroFrameworkPK_v4_3/DeviceCode/Targets/Native/Krait/DeviceCode/Samraksh_SPOT_Hardware/spot_native_Samraksh_SPOT_Hardware_Adapt_AccelerometerInternal.cpp
@@ -0,0 +1,96 @@
+//-----------------------------------------------------------------------------
+//
+//                   ** WARNING! ** 
+//    This file was generated automatically by a tool.
+//    Re-running the tool will overwrite this file.
+//    You should copy this file to a custom location
+//    before adding any customization in the copy to
+//    prevent loss of your changes when the tool is
+//    re-run.
+//
+//-----------------------------------------------------------------------------
+
+
+#include "spot_native.h"
+#include "spot_native_Samraksh_SPOT_Hardware_Adapt_AccelerometerInternal.h"
+
+using namespace Samraksh_SPOT_Hardware_Adapt;
+
+INT16 AccelerometerInternal::ADAPT_Accel_Init( CLR_RT_HeapBlock* pMngObj, HRESULT &hr )
+{
+    INT16 retVal = 0; 
+    return retVal;
+}
+
+INT16 AccelerometerInternal::ADAPT_Accel_GetX( CLR_RT_HeapBlock* pMngObj, HRESULT &hr )
+{
+    INT16 retVal = 0; 
+    return retVal;
+}
+
+INT16 AccelerometerInternal::ADAPT_Accel_GetY( CLR_RT_HeapBlock* pMngObj, HRESULT &hr )
+{
+    INT16 retVal = 0; 
+    return retVal;
+}
+
+INT16 AccelerometerInternal::ADAPT_Accel_GetZ( CLR_RT_HeapBlock* pMngObj, HRESULT &hr )
+{
+    INT16 retVal = 0; 
+    return retVal;
+}
+
+INT16 AccelerometerInternal::ADAPT_Accel_GetTemperature( CLR_RT_HeapBlock* pMngObj, HRESULT &hr )
+{
+    INT16 retVal = 0; 
+    return retVal;
+}
+
+INT16 AccelerometerInternal::ADAPT_Accel_reset( CLR_RT_HeapBlock* pMngObj, HRESULT &hr )
+{
+    INT16 retVal = 0; 
+    return retVal;
+}
+
+INT16 AccelerometerInternal::ADAPT_Accel_advconfig( CLR_RT_HeapBlock* pMngObj, UINT32 param0, HRESULT &hr )
+{
+    INT16 retVal = 0; 
+    return retVal;
+}
+
+INT16 AccelerometerInternal::ADAPT_Accel_SelfTest( CLR_RT_HeapBlock* pMngObj, HRESULT &hr )
+{
+    INT16 retVal = 0; 
+    return retVal;
+}
+
+INT16 AccelerometerInternal::ADAPT_Accel_SetPowerMode( CLR_RT_HeapBlock* pMngObj, UINT32 param0, HRESULT &hr )
+{
+    INT16 retVal = 0; 
+    return retVal;
+}
+
+INT16 AccelerometerInternal::ADAPT_Accel_SetBandwidth( CLR_RT_HeapBlock* pMngObj, UINT32 param0, HRESULT &hr )
+{
+    INT16 retVal = 0; 
+    return retVal;
+}
+
+INT16 AccelerometerInternal::ADAPT_Accel_SetRange( CLR_RT_HeapBlock* pMngObj, UINT16 param0, HRESULT &hr )
+{
+    INT16 retVal = 0; 
+    return retVal;
+}
+
+UINT16 AccelerometerInternal::ADAPT_Accel_Raw_Read( CLR_RT_HeapBlock* pMngObj, UINT16 param0, HRESULT &hr )
+{
+    UINT16 retVal = 0; 
+    return retVal;
+}
+
+INT16 AccelerometerInternal::ADAPT_Accel_Raw_Write( CLR_RT_HeapBlock* pMngObj, UINT8 param0, UINT16 param1, HRESULT &hr )
+{
+    INT16 retVal = 0; 
+    return retVal;
+}
+
