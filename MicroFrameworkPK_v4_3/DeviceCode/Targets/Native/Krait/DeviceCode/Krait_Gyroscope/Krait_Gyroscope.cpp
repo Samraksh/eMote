483d3b7578a60c0594c33a83c36c42feb045413f
ChrisAtSamraksh <chris.morse@samraksh.com>
2014-03-06 16:36:35 -0500

    More namespace fixes

--- MicroFrameworkPK_v4_3/DeviceCode/Targets/Native/Krait/DeviceCode/Krait_Gyroscope/Krait_Gyroscope.cpp
+++ MicroFrameworkPK_v4_3/DeviceCode/Targets/Native/Krait/DeviceCode/Krait_Gyroscope/Krait_Gyroscope.cpp
@@ -9,79 +9,85 @@
 #include "..\Krait.h"
 #include "Krait_Gyroscope.h"
 
-INT8 Initialize()
+INT8 gyro_init()
 {
     INT8 retVal = 0;
 	return retVal;
 }
 
-INT8 Reset()
+INT8 gyro_reset()
 {
 	INT8 retVal = 0;
 	return retVal;
 }
 
-INT8 SelfTest()
+INT8 gyro_self_test()
 {
     INT8 retVal = 0;
 	return retVal;
 }
 
-INT16 GetX()
+INT16 gyro_get_X()
 {
     INT16 xVal = 0; 
     return xVal;
 }
 
-INT16 GetY()
+INT16 gyro_get_Y()
 {
     INT16 yVal = 0; 
     return yVal;
 }
 
-INT16 GetZ()
+INT16 gyro_get_Z()
 {
     INT16 zVal = 0; 
     return zVal;
 }
 
-INT16 GetTemperature()
+INT16 gyro_get_temp()
 {
     INT16 temperature = 0;
 	return temperature;
 }
 
-UINT8 ReadRegister(UINT8 reg)
+UINT8 gyro_raw_read(UINT8 reg)
 {
     UINT8 data = 0;
     return data;
 }
 
-INT8 WriteRegister( UINT8 reg, UINT8 data )
+INT8 gyro_raw_write( UINT8 reg, UINT8 data )
 {
 	INT8 retVal = 0;
 	return retVal;
 }
 
-INT8 AdvancedConfiguration( UINT32 config )
+INT8 gyro_config( UINT32 config )
 {
     INT8 retVal = 0;
 	return retVal;
 }
 
-INT8 SetOffsets(INT16 xOffset, INT16 yOffset, INT16 zOffset)
+INT8 gyro_offsets(INT16 xOffset, INT16 yOffset, INT16 zOffset)
 {
 	INT8 retVal = 0;
 	return retVal;
 }
 
-INT8 SampleConfiguration(UINT32 config)
+INT8 gyro_sample_config(UINT32 config)
 {
 	INT8 retVal = 0;
 	return retVal;
 }
 
-INT8 ADAPT_Accel_GetAll( INT16 data[] )
+INT8 gyro_power( UINT32 mode )
+{
+	INT8 retVal = 0;
+	return retVal;
+}
+
+INT8 gyro_get_XYZ( INT16 data[] )
 {
     INT8 retVal = 0;
 	
--- MicroFrameworkPK_v4_3/DeviceCode/Targets/Native/Krait/DeviceCode/Krait_Gyroscope/Krait_Gyroscope.h
+++ MicroFrameworkPK_v4_3/DeviceCode/Targets/Native/Krait/DeviceCode/Krait_Gyroscope/Krait_Gyroscope.h
@@ -8,19 +8,20 @@
 #ifndef KRAIT_GYROSCOPE_H
 #define KRAIT_GYROSCOPE_H
 
-INT8 Initialize();
-INT8 Reset();
-INT8 SelfTest();
-INT16 GetX();
-INT16 GetY();
-INT16 GetZ();
-INT16 GetTemperature();
-UINT8 ReadRegister(UINT8 reg);
-INT8 WriteRegister(UINT8 reg, UINT8 data);
-INT8 AdvancedConfiguration(UINT32 config);
-INT8 SetOffsets(INT16 xOffset, INT16 yOffset, INT16 zOffset);
-INT8 SampleConfiguration(UINT32 config);
-INT8 GetAllData(INT16 data[]);
+INT8 gyro_init();
+INT8 gyro_reset();
+INT8 gyro_self_test();
+INT16 gyro_get_X();
+INT16 gyro_get_Y();
+INT16 gyro_get_Z();
+INT16 gyro_get_temp();
+UINT8 gyro_raw_read(UINT8 reg);
+INT8 gyro_raw_write(UINT8 reg, UINT8 data);
+INT8 gyro_config(UINT32 config);
+INT8 gyro_offsets(INT16 xOffset, INT16 yOffset, INT16 zOffset);
+INT8 gyro_sample_config(UINT32 config);
+INT8 gyro_power( UINT32 mode );
+INT8 gyro_get_XYZ(INT16 data[]);
 
 #endif // KRAIT_GYROSCOPE_H
 
