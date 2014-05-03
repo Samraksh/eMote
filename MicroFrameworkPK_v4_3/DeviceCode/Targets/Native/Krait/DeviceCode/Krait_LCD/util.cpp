2c188275c17bea6ad21b9fe259138ba844c295b2
Ananth Muralidharan <ananth.muralidharan@samraksh.com>
2014-05-02 20:24:09 -0400

    -Initial checkin for developer; Removed Samraksh specific source files from deviceCode/drivers folder and dll source code -Modified MSFT.Targets file such that when clean is called staticLibs folder is not removed; Modified batch script such that lib files are not copied over to staticLibs folder -Removed some more cpp files

--- MicroFrameworkPK_v4_3/DeviceCode/Targets/Native/Krait/DeviceCode/Krait_LCD/util.cpp
+++
@@ -1,7 +0,0 @@
-#include <tinyhal.h>
-
-UINT32 ntohl(UINT32 input)
-{
-	input = ((input << 8) & 0xFF00FF00) | ((input >> 8) & 0xFF00FF );
-	return (input << 16 | input >> 16);
-}
---
+++ MicroFrameworkPK_v4_3/tools/Scripts/PrepareStaticLibsForConcat.cmd
@@ -0,0 +1,32 @@
+@echo off
+
+REM 
+REM This script is used to extract a list of libraries under a .\tmp directory
+REM
+
+set PREVCD=%CD%
+
+
+for %%i IN (%*) do (
+  IF "%%i"=="%1" (
+	IF NOT EXIST %1\tmp mkdir %1\tmp
+	cd %1\tmp
+	REM copy ..\..\lib\*.lib %1
+  ) 
+)
+
+for %%i IN (..\*.lib) do (
+      IF EXIST "%GNU_TOOLS_BIN%\%GNU_TARGET%-ar.exe" (
+		@echo %GNU_TOOLS_BIN%\%GNU_TARGET%-ar -x %%i
+        %GNU_TOOLS_BIN%\%GNU_TARGET%-ar -x %%i
+      ) ELSE (
+          @echo ERROR: Cannot find GCC archiver tool!
+          GOTO :DONE
+        )
+    )
+
+
+:DONE
+
+cd %PREVCD%
+set PREVCD=
