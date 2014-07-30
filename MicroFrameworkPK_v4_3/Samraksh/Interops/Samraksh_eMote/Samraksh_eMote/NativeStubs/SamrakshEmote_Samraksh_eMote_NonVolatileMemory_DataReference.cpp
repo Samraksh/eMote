913bb08dff9f6e873cebb3669242c20c689aa542
ChrisAtSamraksh <chris.morse@samraksh.com>
2014-07-30 14:22:00 -0400

    eMote dll updates with XML files included.
    The Samraksh dlls were recompiled to include XML files to be used in documentation.

--- MicroFrameworkPK_v4_3/Samraksh/Interops/Samraksh_eMote/Samraksh_eMote/NativeStubs/SamrakshEmote_Samraksh_eMote_NonVolatileMemory_DataReference.cpp
+++ MicroFrameworkPK_v4_3/Samraksh/Interops/Samraksh_eMote/Samraksh_eMote/NativeStubs/SamrakshEmote_Samraksh_eMote_NonVolatileMemory_DataReference.cpp
@@ -13,75 +13,60 @@
 
 #include "SamrakshEmote.h"
 #include "SamrakshEmote_Samraksh_eMote_NonVolatileMemory_DataReference.h"
-#include <Samraksh\DataStore\Datastore.h>
 
 using namespace Samraksh::eMote::NonVolatileMemory;
 
-extern Data_Store g_dataStoreObject;
-static UINT32 dataID;
-
-INT32 DataReference::GetDataReference( CLR_RT_HeapBlock* pMngObj, UINT32 dataId, HRESULT &hr )
+INT32 DataReference::GetDataReference( CLR_RT_HeapBlock* pMngObj, UINT32 param0, HRESULT &hr )
 {
-	return (INT32)g_dataStoreObject.getAddress(dataId);
+    INT32 retVal = 0; 
+    return retVal;
 }
 
-INT32 DataReference::LookupDataType( CLR_RT_HeapBlock* pMngObj, UINT32 dataId, HRESULT &hr )
+INT32 DataReference::LookupDataType( CLR_RT_HeapBlock* pMngObj, UINT32 param0, HRESULT &hr )
 {
-	return (INT32)g_dataStoreObject.getDataType(dataId);
+    INT32 retVal = 0; 
+    return retVal;
 }
 
-INT32 DataReference::LookupDataSize( CLR_RT_HeapBlock* pMngObj, UINT32 dataId, HRESULT &hr )
+INT32 DataReference::LookupDataSize( CLR_RT_HeapBlock* pMngObj, UINT32 param0, HRESULT &hr )
 {
-    return (INT32)g_dataStoreObject.getAllocationSize(dataId);
+    INT32 retVal = 0; 
+    return retVal;
 }
 
-INT8 DataReference::DeleteData( CLR_RT_HeapBlock* pMngObj, UINT32 dataId, HRESULT &hr )
+INT8 DataReference::DeleteData( CLR_RT_HeapBlock* pMngObj, UINT32 param0, HRESULT &hr )
 {
-    g_dataStoreObject.deleteRecord(dataId);
-    if(g_dataStoreObject.getLastError() == DATASTORE_ERROR_NONE)
-		return true;
-	else
-		return false;
+    INT8 retVal = 0; 
+    return retVal;
 }
 
-INT32 DataReference::CreateData( CLR_RT_HeapBlock* pMngObj, UINT32 numBytes, UINT8 dataType, HRESULT &hr )
+INT32 DataReference::CreateData( CLR_RT_HeapBlock* pMngObj, UINT32 param0, UINT8 param1, HRESULT &hr )
 {
-	if(dataID == 0)
-		dataID = g_dataStoreObject.getRecentRecordID();
-
-    dataID++;
-	return  (int)g_dataStoreObject.createRecord(dataID, numBytes, dataType);
-    //return  (int)g_dataStoreObject.createRecord(dataID, numBytes, 0);
+    INT32 retVal = 0; 
+    return retVal;
 }
 
-INT32 DataReference::CreateData( CLR_RT_HeapBlock* pMngObj, UINT32 numBytes, UINT16 dataType, HRESULT &hr )
+INT32 DataReference::CreateData( CLR_RT_HeapBlock* pMngObj, UINT32 param0, UINT16 param1, HRESULT &hr )
 {
-	if(dataID == 0)
-		dataID = g_dataStoreObject.getRecentRecordID();
-
-    dataID++;
-	return  (int)g_dataStoreObject.createRecord(dataID, numBytes, dataType);
+    INT32 retVal = 0; 
+    return retVal;
 }
 
-INT32 DataReference::CreateData( CLR_RT_HeapBlock* pMngObj, UINT32 numBytes, UINT32 dataType, HRESULT &hr )
+INT32 DataReference::CreateData( CLR_RT_HeapBlock* pMngObj, UINT32 param0, UINT32 param1, HRESULT &hr )
 {
-	if(dataID == 0)
-		dataID = g_dataStoreObject.getRecentRecordID();
-
-	dataID++;
-	return  (int)g_dataStoreObject.createRecord(dataID, numBytes, dataType);
+    INT32 retVal = 0; 
+    return retVal;
 }
 
 UINT32 DataReference::GetDataID( CLR_RT_HeapBlock* pMngObj, HRESULT &hr )
 {
-	if(dataID == 0)
-		dataID = g_dataStoreObject.getRecentRecordID();
-
-    return dataID;
+    UINT32 retVal = 0; 
+    return retVal;
 }
 
-INT8 DataReference::DisposeNativeMemoryPointer( CLR_RT_HeapBlock* pMngObj, UINT32 dataId, HRESULT &hr )
+INT8 DataReference::DisposeNativeMemoryPointer( CLR_RT_HeapBlock* pMngObj, UINT32 param0, HRESULT &hr )
 {
-    return g_dataStoreObject.deleteRecord(dataId);
+    INT8 retVal = 0; 
+    return retVal;
 }
 
