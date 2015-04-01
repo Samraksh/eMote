728e54bdc3294f08172db5fb20a0f5b9dc7453b5
MichaelAtSamraksh <Michael.McGrath@samraksh.com>
2015-03-31 20:03:11 -0400

    .NOW INTC: add global lock around SystemState in exti1 handler

--- MicroFrameworkPK_v4_3/DeviceCode/Targets/Native/STM32F10x/DeviceCode/drivers/intc/STM32__AITC.cpp
+++ MicroFrameworkPK_v4_3/DeviceCode/Targets/Native/STM32F10x/DeviceCode/drivers/intc/STM32__AITC.cpp
@@ -543,8 +543,10 @@ void HardFault_HandlerC(unsigned long *hardfault_args)
 		STM32_AITC& AITC = STM32::AITC();
 
 		// set before jumping elsewhere or allowing other interrupts
+		GLOBAL_LOCK(irq);
 		SystemState_SetNoLock( SYSTEM_STATE_ISR              );
 		SystemState_SetNoLock( SYSTEM_STATE_NO_CONTINUATIONS );
+		irq.Release();
 		
 #ifdef DEBUG_DOTNOW_ISR
 		interrupt_count[c_IRQ_INDEX_EXTI1]++;
@@ -554,7 +556,7 @@ void HardFault_HandlerC(unsigned long *hardfault_args)
 
 		IsrVector->Handler.Execute();
 
-
+		irq.Acquire();
 		SystemState_ClearNoLock( SYSTEM_STATE_NO_CONTINUATIONS ); // nestable
 		SystemState_ClearNoLock( SYSTEM_STATE_ISR              ); // nestable
 	}
