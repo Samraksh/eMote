5a3b0241f1c5a09cbefb5555e102ecc3c8ee41b0
Bora Karaoglu <bora.karaoglu@samraksh.com>
2015-04-24 15:27:17 -0400

    Adding missing OMAC headers

---
+++ MicroFrameworkPK_v4_3/DeviceCode/pal/COM/Wireless/MAC/OMAC/DataReceptionHandler.h
@@ -0,0 +1,53 @@
+/*
+ * DataReceptionHandler.h
+ *
+ *  Created on: Apr 30, 2013
+ *      Author: Mukundan
+ */
+
+#include <Samraksh/Message.h>
+//#include <Samraksh/HALTimer.h>
+#include "OMACConstants.h"
+#include "Handlers.h"
+
+#ifndef DATARECEPTIONHANDLER_H_
+#define DATARECEPTIONHANDLER_H_
+
+
+class DataReceptionHandler: public SlotHandler {
+	BOOL m_busy, m_shldWakeup;
+	UINT32 m_dataInterval;
+	/* the source node we're expecting during dwell time, to filter out messages, accidentally sent
+	 * back to back by another sender, that stops our dwell time */
+	UINT16 	m_dwellSource;
+	/*for calculateConversionTask*/
+
+	Message_15_4_t 	dataBeaconBuffer, receiveMsgBuffer;
+	//Message_15_4_t 	debugMsg;
+	Message_15_4_t  *dataBeaconBufferPtr;
+	//UINT8 m_dwellCnt ;
+	bool m_receivedDataPacket, m_efdDetected;
+	UINT16 	m_wakeupCnt, m_idleListenCnt, m_collisionCnt, m_overhearCnt;
+	UINT16	m_sfdCnt;
+	//wakeup slots that receive at least one packet
+	UINT16 	m_receivedSlotCnt, m_reportPeriod;
+
+	UINT32	m_lastBeaconRequestSlot;
+	UINT32	m_nextWakeupSlot, m_seedUpdateInterval;
+	UINT16	m_nextSeed, m_mask;
+
+	UINT32	wakeupSlot, wakeupTime, scheduledWakeupTime;
+	//SeedGenerator m_seedGenerator;
+
+public:
+	void Initialize();
+	UINT16 NextSlot(UINT32 slotNum);
+	void ExecuteSlot(UINT32 slotNum);
+	UINT8 ExecuteSlotDone();
+	void PostExecuteSlot();
+	void SetWakeup(bool shldWakeup);
+	bool SendDataBeacon(bool sendPiggyback);
+};
+
+
+#endif /* DATARECEPTIONHANDLER_H_ */
---
+++ MicroFrameworkPK_v4_3/DeviceCode/pal/COM/Wireless/MAC/OMAC/DataTransmissionHandler.h
@@ -0,0 +1,48 @@
+/*
+ * DataTransmissionHandler.h
+ *
+ *  Created on: Apr 30, 2013
+ *      Author: Mukundan
+ */
+
+#ifndef DATATRANSMISSIONHANDLER_H_
+#define DATATRANSMISSIONHANDLER_H_
+
+#include <tinyhal.h>
+#include <Samraksh/Message.h>
+//#include <Samraksh/HALTimer.h>
+#include "OMACConstants.h"
+#include "Handlers.h"
+
+
+class DataTransmissionHandler: public SlotHandler {
+	UINT8   m_dsn;
+	UINT8   m_retryCnt, m_dwellCnt;
+	UINT16  randVal;
+	UINT16 m_nextDestination;
+	bool m_receivedDataBeacon;
+	bool   m_busy;   //indicates if radio is busy.
+
+	  //the # of times the current packet has been sent
+	UINT8   m_dataHeartBeats;
+	UINT32  m_nextTXCounter;
+	UINT64  m_nextTXTicks;
+	UINT32  m_lastSlot;
+	Message_15_4_t m_piggybackBeaconCopy;
+	Message_15_4_t* m_outgoingEntryPtr;
+	UINT32  m_collisionCnt;
+
+
+
+public:
+	void Initialize();
+	UINT16 NextSlot(UINT32 slotNum);
+	void ExecuteSlot(UINT32 slotNum);
+	UINT8 ExecuteSlotDone();
+	void PostExecuteSlot();
+	void SetWakeup(bool shldWakeup);
+	bool IsSynced(UINT16 nbr);
+	void DataBeaconReceive(UINT8 type, Message_15_4_t *msg, UINT8 size);
+	void ScheduleDataPacket();
+};
+#endif /* DATATRANSMISSIONHANDLER_H_ */
