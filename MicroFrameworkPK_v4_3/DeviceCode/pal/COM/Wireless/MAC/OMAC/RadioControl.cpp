7b2d817ea1a6a058468d3d5db1101d1227d2409e
Bora Karaoglu <bora.karaoglu@samraksh.com>
2015-05-04 17:35:22 -0400

    Re-enabling sleeping

--- MicroFrameworkPK_v4_3/DeviceCode/pal/COM/Wireless/MAC/OMAC/RadioControl.cpp
+++ MicroFrameworkPK_v4_3/DeviceCode/pal/COM/Wireless/MAC/OMAC/RadioControl.cpp
@@ -112,7 +112,7 @@ DeviceStatus RadioControl::Send_TimeStamped(RadioAddress_t address, Message_15_4
 
 DeviceStatus RadioControl::Stop(){
 	DeviceStatus returnVal = DS_Success;
-	//DeviceStatus returnVal = CPU_Radio_Sleep(g_OMAC.radioName,0);
+	DeviceStatus returnVal = CPU_Radio_Sleep(g_OMAC.radioName,0);
 	return returnVal;
 }
 
