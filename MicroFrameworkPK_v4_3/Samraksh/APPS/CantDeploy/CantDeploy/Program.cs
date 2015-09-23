854b966a6525a7f6f8735672ecc17aec39dc8a3f
Nathan Stohs <nathan.stohs@samraksh.com>
2015-09-23 14:43:07 -0400

    C# app for testing code that will stop deployment.

---
+++ MicroFrameworkPK_v4_3/Samraksh/APPS/CantDeploy/CantDeploy/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+
+
+// Example code that will block deployment.
+// Used for testing and trying to fix.
+// This one is cheating and a little trivial, but its a start.
+
+namespace CantDeploy
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // Can only deploy during this pause
+            Thread.Sleep(15000);
+
+            Debug.Print("Test. Can you still re-deploy app?");
+
+            // Go to low power mode where TinyCLR is too slow to talk to the PC
+            PowerState.ChangePowerLevel(PowerLevel.Low);
+
+            // Done
+            Thread.Sleep(Timeout.Infinite);
+        }
+
+    }
+}
