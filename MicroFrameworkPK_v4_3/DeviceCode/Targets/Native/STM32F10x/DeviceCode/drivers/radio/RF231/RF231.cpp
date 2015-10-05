Samraksh/eMote

commit 4cf4329b3a599a884df56f0d258f858b33586de7

.NOW SPI: Band-aid fixes for clock control issues.
See #335.

-Prevent the SPI driver from unilaterally changing clock dividers.

-Pick "safe" settings for 64 MHz as defaults until something more
substansial can be worked out.

MicroFrameworkPK_v4_3/DeviceCode/Targets/Native/STM32F10x/DeviceCode/drivers/radio/RF231/RF231.cpp
@@ -1026,7 +1026,7 @@ BOOL RF231Radio::SpiInitialize()
 	config.CS_Hold_uSecs          = 0;
 	config.MSK_IDLE               = false;
 	config.MSK_SampleEdge         = false;
-	config.Clock_RateKHz          = 2;
+	config.Clock_RateKHz          = 16;
 	if(this->GetRadioName() == RF231RADIO)
 	{
 		config.SPI_mod                = SPIBUS1;

MicroFrameworkPK_v4_3/DeviceCode/Targets/Native/STM32F10x/DeviceCode/drivers/spi/netmf_spi.cpp
@@ -166,7 +166,7 @@ void CPU_SPI_Enable(SPI_CONFIGURATION config)
 	if(config.SPI_mod == SPIBUS1)
 	{
 
-		RCC_PCLK2Config(RCC_HCLK_Div2);
+		//RCC_PCLK2Config(RCC_HCLK_Div2);
 		RCC_APB2PeriphClockCmd(SPIx_GPIO_CLK | SPIx_CLK, ENABLE);
 
 		GPIO_InitTypeDef GPIO_InitStructure;
@@ -183,7 +183,7 @@ void CPU_SPI_Enable(SPI_CONFIGURATION config)
 		SPI_InitStructure.SPI_CPOL = 0;
 		SPI_InitStructure.SPI_CPHA = 0;
 		SPI_InitStructure.SPI_NSS = SPI_NSS_Hard;
-		SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_4;
+		SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_16;
 		SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_MSB;
 		SPI_InitStructure.SPI_CRCPolynomial = 7;
 
@@ -194,8 +194,8 @@ void CPU_SPI_Enable(SPI_CONFIGURATION config)
 	else if(config.SPI_mod == SPIBUS2)
 	{
 
-		RCC_PCLK1Config(RCC_HCLK_Div2);
-		RCC_PCLK2Config(RCC_HCLK_Div2);
+		//RCC_PCLK1Config(RCC_HCLK_Div2);
+		//RCC_PCLK2Config(RCC_HCLK_Div2);
 		RCC_APB2PeriphClockCmd(SPIy_GPIO_CLK, ENABLE);
 		RCC_APB1PeriphClockCmd(SPIy_CLK, ENABLE);
 
@@ -216,7 +216,7 @@ void CPU_SPI_Enable(SPI_CONFIGURATION config)
 		SPI_InitStructure.SPI_CPOL = 0;
 		SPI_InitStructure.SPI_CPHA = 0;
 		SPI_InitStructure.SPI_NSS = SPI_NSS_Hard;
-		SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_4;
+		SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_16;
 		SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_MSB;
 		SPI_InitStructure.SPI_CRCPolynomial = 7;
 
@@ -837,7 +837,7 @@ void GPIO_Config(const SPI_CONFIGURATION& Configuration)
 void RCC_Config()
 {
   /* PCLK2 = HCLK/2 */
-  RCC_PCLK2Config(RCC_HCLK_Div2);
+  //RCC_PCLK2Config(RCC_HCLK_Div2);
   
   /* Enable SPIx clock and GPIO clock for SPIx and SPIy */
   RCC_APB2PeriphClockCmd(SPIx_GPIO_CLK | SPIy_GPIO_CLK | SPIx_CLK, ENABLE);

commit d03c80f728da5b4f93fecfde2c54cb92a66f0077

.NOW SPI: See if this fixes broken RF231 test.
Chris reported 4cf4329b broke things. Not sure why. Lets poke it.

MicroFrameworkPK_v4_3/DeviceCode/Targets/Native/STM32F10x/DeviceCode/drivers/radio/RF231/RF231.cpp
@@ -1026,7 +1026,7 @@ BOOL RF231Radio::SpiInitialize()
 	config.CS_Hold_uSecs          = 0;
 	config.MSK_IDLE               = false;
 	config.MSK_SampleEdge         = false;
-	config.Clock_RateKHz          = 16;
+	config.Clock_RateKHz          = 16; // THIS IS IGNORED.
 	if(this->GetRadioName() == RF231RADIO)
 	{
 		config.SPI_mod                = SPIBUS1;

MicroFrameworkPK_v4_3/DeviceCode/Targets/Native/STM32F10x/DeviceCode/drivers/spi/netmf_spi.cpp
@@ -183,7 +183,7 @@ void CPU_SPI_Enable(SPI_CONFIGURATION config)
 		SPI_InitStructure.SPI_CPOL = 0;
 		SPI_InitStructure.SPI_CPHA = 0;
 		SPI_InitStructure.SPI_NSS = SPI_NSS_Hard;
-		SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_16;
+		SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_8;
 		SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_MSB;
 		SPI_InitStructure.SPI_CRCPolynomial = 7;
 
@@ -216,7 +216,7 @@ void CPU_SPI_Enable(SPI_CONFIGURATION config)
 		SPI_InitStructure.SPI_CPOL = 0;
 		SPI_InitStructure.SPI_CPHA = 0;
 		SPI_InitStructure.SPI_NSS = SPI_NSS_Hard;
-		SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_16;
+		SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_8;
 		SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_MSB;
 		SPI_InitStructure.SPI_CRCPolynomial = 7;
 
@@ -764,6 +764,8 @@ void SPI_StructInit(const SPI_CONFIGURATION& Configuration)
   
   /* Initialize the SPI_BaudRatePrescaler member */
   //FIXME: Right now we are pre-scaling map it to configuration clock
+  SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_8;
+  /*
   switch (Configuration.Clock_RateKHz)
   {
   case 2:
@@ -793,7 +795,8 @@ void SPI_StructInit(const SPI_CONFIGURATION& Configuration)
   default:
 	  SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_2;
 	  break;
-  }   
+  }
+  */
   /* Initialize the SPI_FirstBit member */
 #if defined(FIRST_BIT_LSB)
   SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_LSB;