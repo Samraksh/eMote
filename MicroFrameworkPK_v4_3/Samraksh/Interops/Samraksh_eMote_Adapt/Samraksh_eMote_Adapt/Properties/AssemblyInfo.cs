fbfe4bd0e3c32c2a1840bb67654b567b937bd984
AnanthAtSamraksh <Ananth.Muralidharan@samraksh.info>
2015-10-12 12:30:53 -0400

    Checks in Samraksh dlls modified for netmf 4.3.2.0

--- MicroFrameworkPK_v4_3/Samraksh/Interops/Samraksh_eMote_Adapt/Samraksh_eMote_Adapt/Properties/AssemblyInfo.cs
+++ MicroFrameworkPK_v4_3/Samraksh/Interops/Samraksh_eMote_Adapt/Samraksh_eMote_Adapt/Properties/AssemblyInfo.cs
@@ -19,5 +19,5 @@
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("4.3.1.12")]
-[assembly: AssemblyFileVersion("4.3.1.12")]
+[assembly: AssemblyVersion("4.3.2.0")]
+[assembly: AssemblyFileVersion("4.3.2.0")]
