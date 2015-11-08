4b44208bef0595772aa0cbf7487b79141dc9c501
ChrisAtSamraksh <chris.morse@samraksh.com>
2015-11-08 13:53:40 -0500

    More files to checkin

---
+++ MicroFrameworkPK_v4_3/DeviceCode/pal/Samraksh/Samraksh_eMote/SamrakshEmote_Samraksh_eMote_Algorithm__AcousticDetection.cpp
@@ -0,0 +1,39 @@
+//-----------------------------------------------------------------------------
+//
+//                   ** WARNING! ** 
+//    This file was generated automatically by a tool.
+//    Re-running the tool will overwrite this file.
+//    You should copy this file to a custom location
+//    before adding any customization in the copy to
+//    prevent loss of your changes when the tool is
+//    re-run.
+//
+//-----------------------------------------------------------------------------
+
+
+#include "SamrakshEmote.h"
+#include "SamrakshEmote_Samraksh_eMote_Algorithm__AcousticDetection.h"
+#include "Samraksh\Acoustic.h"
+
+using namespace Samraksh::eMote;
+static INT32 Pa = 1, Pm = 1;
+
+INT8 Algorithm_AcousticDetection::DetectionCalculation( CLR_RT_HeapBlock* pMngObj, CLR_RT_TypedArray_UINT16 param0, INT32 param1, CLR_RT_TypedArray_double param2, INT8 param3, HRESULT &hr )
+{
+    INT8 retVal = 0; 
+
+	retVal = testFunction(param0.GetBuffer(), param1, param2.GetBuffer(), param3, Pa, Pm);
+
+    return retVal;
+}
+
+INT8 Algorithm_AcousticDetection::SetDetectionParameters( CLR_RT_HeapBlock* pMngObj, INT32 param0, INT32 param1, HRESULT &hr )
+{
+    INT8 retVal = 0; 
+
+	Pa = param0;
+	Pm = param1;
+
+    return retVal;
+}
+
