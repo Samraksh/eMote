b226cc44f71d9689e7757fc13125198b454112b1
Ananth Muralidharan <ananth.muralidharan@samraksh.com>
2016-01-13 13:10:33 -0500

    No debugs(printf or GPIO) in OMAC by default

--- MicroFrameworkPK_v4_3/DeviceCode/Include/Samraksh/MAC/OMAC/OMAC.h
+++ MicroFrameworkPK_v4_3/DeviceCode/Include/Samraksh/MAC/OMAC/OMAC.h
@@ -9,12 +9,12 @@
 #ifndef OMAC_H_
 #define OMAC_H_
 
-#define TWO_NODES_TX_RX
-#define OMAC_DEBUG_GPIO
+//#define TWO_NODES_TX_RX
+//#define OMAC_DEBUG_GPIO
 //#define OMAC_DEBUG_PRINTF
 
 #if defined(TWO_NODES_TX_RX)
-#define def_Neighbor2beFollowed
+//#define def_Neighbor2beFollowed
 #define OMAC_DEBUG_GPIO
 /* // Nathan's nodes
 //#define TXNODEID 18134
@@ -22,11 +22,11 @@
 */
 //Bora's Nodes
 //#define TXNODEID 30906
-#define TXNODEID 4028
-#define RXNODEID 32696
+//#define TXNODEID 4028
+//#define RXNODEID 32696
 // Ananth's Nodes
-//#define TXNODEID 3505
-//#define RXNODEID 6846
+#define TXNODEID 3505
+#define RXNODEID 6846
 #endif
 
 #include <Samraksh/MAC.h>
