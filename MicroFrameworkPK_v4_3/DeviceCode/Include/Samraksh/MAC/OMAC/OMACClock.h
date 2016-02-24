0f015eb937901f9fe74e579f8ca71fd4cfbf2905
Bora Karaoglu <bora.karaoglu@samraksh.com>
2016-02-24 14:50:13 -0500

    OMAC: Additional functions for OMACClock

--- MicroFrameworkPK_v4_3/DeviceCode/Include/Samraksh/MAC/OMAC/OMACClock.h
+++ MicroFrameworkPK_v4_3/DeviceCode/Include/Samraksh/MAC/OMAC/OMACClock.h
@@ -9,13 +9,52 @@
 class OMACClock{
 	UINT64 m_inter_clock_offset;
 public:
-	OMACClock();
-	UINT64 GetCurrentTimeinTicks();
-	UINT64 ConvertTickstoMicroSecs(const UINT64& ticks);
-	UINT64 CreateSyncPointBetweenClocks();
+	OMACClock(){
+		m_inter_clock_offset = 0;
+	}
+	UINT64 GetCurrentTimeinTicks(){ //This function gets the time ticks required for OMAC
+		if(OMACClockSpecifier == LFCLOCKID){
+			return VirtTimer_GetTicks(VIRT_TIMER_OMAC_SCHEDULER) * OMACClocktoSystemClockFreqRatio + m_inter_clock_offset;
+		}
+		else{
+			return HAL_Time_CurrentTicks();
+		}
+	};
+	UINT64 ConvertTickstoMicroSecs(const UINT64& ticks){ //This function gets the time ticks required for OMAC
+		//return VirtTimer_GetTicks(VIRT_TIMER_OMAC_SCHEDULER) * OMACClocktoSystemClockFreqRatio;
+		return HAL_Time_TicksToTime(ticks);
+	};
+	UINT64 ConvertMicroSecstoTicks(const UINT64& microsecs){ //This function gets the time ticks required for OMAC
+		//return VirtTimer_GetTicks(VIRT_TIMER_OMAC_SCHEDULER) * OMACClocktoSystemClockFreqRatio;
+		return microsecs*8;
+	};
+	UINT64 ConvertSlotstoTicks(const UINT64& slots){ //This function gets the time ticks required for OMAC
+		//return VirtTimer_GetTicks(VIRT_TIMER_OMAC_SCHEDULER) * OMACClocktoSystemClockFreqRatio;
+		return slots*16000*8;
+	};
+
+	UINT64 CreateSyncPointBetweenClocks(){
+		if(0 && OMACClockSpecifier == LFCLOCKID){
+			m_inter_clock_offset = HAL_Time_CurrentTicks() - VirtTimer_GetTicks(VIRT_TIMER_OMAC_SCHEDULER) * OMACClocktoSystemClockFreqRatio ;
+		}
+	};
+	inline UINT64 AddTicks(const UINT64& ticks1, const UINT64& ticks2){
+		return (ticks1 + ticks2);
+	};
+	inline UINT64 SubstractTicks(const UINT64& ticks1, const UINT64& ticks2){
+		return (ticks1 - ticks2);
+	};
+	inline UINT64 SubstractMicroSeconds(const UINT64& ticks1, const UINT64& ticks2){
+		return (ticks1 - ticks2);
+	};
+	inline UINT64 AddMicroSeconds(const UINT64& ticks1, const UINT64& ticks2){
+		return (ticks1 + ticks2);
+	};
+
 
 };
 
+/*
 OMACClock::OMACClock(){
 	m_inter_clock_offset = 0;
 }
@@ -39,6 +78,6 @@ UINT64 OMACClock::ConvertTickstoMicroSecs(const UINT64& ticks){ //This function 
 	//return VirtTimer_GetTicks(VIRT_TIMER_OMAC_SCHEDULER) * OMACClocktoSystemClockFreqRatio;
 	return HAL_Time_TicksToTime(ticks);
 }
-
+*/
 
 #endif /* OMACCLOK_H_ */
