e2fd90819536e2961af6dd45d41423a42ff7a23b
Ananth Muralidharan <ananth.muralidharan@samraksh.com>
2016-06-03 17:30:23 -0400

    TEMPORARY COMMIT
    - prevents OMAC's modules from instructing the radio to go to sleep.
    - increases discovery by 2x

--- MicroFrameworkPK_v4_3/DeviceCode/pal/COM/Wireless/MAC/OMAC/DataReceptionHandler.cpp
+++ MicroFrameworkPK_v4_3/DeviceCode/pal/COM/Wireless/MAC/OMAC/DataReceptionHandler.cpp
@@ -326,7 +326,8 @@ void DataReceptionHandler::PostExecuteEvent(){
 	}*/
 
 	//Scheduler's PostExecution stops the radio
-	DeviceStatus returnVal = g_OMAC.m_omac_RadioControl.Stop();
+	DeviceStatus returnVal = DS_Success;
+	//DeviceStatus returnVal = g_OMAC.m_omac_RadioControl.Stop();
 	if(returnVal == DS_Success) {
 #ifdef OMAC_DEBUG_GPIO
 	CPU_GPIO_SetPinState( DATARECEPTION_SLOTPIN, FALSE );
