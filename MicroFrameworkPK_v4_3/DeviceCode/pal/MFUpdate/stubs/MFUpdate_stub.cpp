0e68aed0bcb6ecb183321c8577a7691e6339646e
MichaelAtSamraksh <Michael.McGrath@samraksh.com>
2016-07-19 00:00:23 -0400

    MFUpdate: change stubs to new samraksh api

--- MicroFrameworkPK_v4_3/DeviceCode/pal/MFUpdate/stubs/MFUpdate_stub.cpp
+++ MicroFrameworkPK_v4_3/DeviceCode/pal/MFUpdate/stubs/MFUpdate_stub.cpp
@@ -8,63 +8,63 @@ void  MFUpdate_Initialize(void)
 {
 }
 
-BOOL MFUpdate_GetProperty( UINT32 updateHandle, LPCSTR szPropName, UINT8* pPropValue, INT32* pPropValueSize )
+BOOL MFUpdate_GetProperty( UpdateID_t updateHandle, LPCSTR szPropName, UINT8* pPropValue, INT32* pPropValueSize )
 {
     return FALSE;
 }
 
-BOOL MFUpdate_SetProperty( UINT32 updateHandle, LPCSTR szPropName, UINT8* pPropValue, INT32 pPropValueSize )
+BOOL MFUpdate_SetProperty( UpdateID_t updateHandle, LPCSTR szPropName, UINT8* pPropValue, INT32 pPropValueSize )
 {
     return FALSE;
 }
 
-INT32 MFUpdate_InitUpdate( LPCSTR szProvider, MFUpdateHeader& update )
+UpdateID_t MFUpdate_InitUpdate( LPCSTR szProvider, MFUpdateHeader& update )
 {
-    return -1;
+    return MFUpdate::badHandle;
 }
 
-BOOL MFUpdate_AuthCommand( INT32 updateHandle, UINT32 cmd, UINT8* pArgs, INT32 argsLen, UINT8* pResponse, INT32& responseLen )
-{
-    return FALSE;
-}
-
-BOOL MFUpdate_Authenticate( INT32 updateHandle, UINT8* pAuthData, INT32 authLen )
+BOOL MFUpdate_AuthCommand( UpdateID_t updateHandle, UINT32 cmd, UINT8* pArgs, INT32 argsLen, UINT8* pResponse, INT32& responseLen )
 {
     return FALSE;
 }
 
-
-BOOL MFUpdate_Open( INT32 updateHandle )
+BOOL MFUpdate_Authenticate( UpdateID_t updateHandle, UINT8* pAuthData, INT32 authLen )
 {
     return FALSE;
 }
 
-BOOL MFUpdate_Create( INT32 updateHandle )
+
+BOOL MFUpdate_Open( UpdateID_t updateHandle )
 {
     return FALSE;
 }
 
-BOOL MFUpdate_GetMissingPackets( INT32 updateHandle, UINT32* pPacketBits, INT32* pCount )
+BOOL MFUpdate_Create( UpdateID_t updateHandle )
 {
     return FALSE;
 }
 
-BOOL MFUpdate_AddPacket( INT32 updateHandle, INT32 packetIndex, UINT8* packetData, INT32 packetLen, UINT8* pValidationData, INT32 validationLen )
+BOOL MFUpdate_GetMissingPackets( UpdateID_t updateHandle, UINT32* pPacketBits, INT32* pCount )
 {
     return FALSE;
 }
 
-BOOL MFUpdate_Validate( INT32 updateHandle, UINT8* pValidationData, INT32 validationLen )
+BOOL MFUpdate_AddPacket( UpdateID_t updateHandle, INT32 packetIndex, UINT8* packetData, INT32 packetLen, UINT8* pValidationData, INT32 validationLen )
 {
     return FALSE;
 }
 
-BOOL MFUpdate_Install( INT32 updateHandle, UINT8* pValidationData, INT32 validationLen )
+BOOL MFUpdate_Validate( UpdateID_t updateHandle, UINT8* pValidationData, INT32 validationLen )
 {
     return FALSE;
 }
 
-BOOL MFUpdate_Delete( INT32 updateHandle )
+BOOL MFUpdate_Install( UpdateID_t updateHandle, UINT8* pValidationData, INT32 validationLen )
+{
+    return FALSE;
+}
+
+BOOL MFUpdate_Delete( UpdateID_t updateHandle )
 {
     return FALSE;
 }
