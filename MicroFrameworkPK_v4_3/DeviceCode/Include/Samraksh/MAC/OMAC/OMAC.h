a4dc424b780172a6c935c26021fa631baa6e73df
ChrisAtSamraksh <chris.morse@samraksh.com>
2016-09-14 16:42:40 -0400

    A few changes from master.

--- MicroFrameworkPK_v4_3/DeviceCode/Include/Samraksh/MAC/OMAC/OMAC.h
+++ MicroFrameworkPK_v4_3/DeviceCode/Include/Samraksh/MAC/OMAC/OMAC.h
@@ -15,7 +15,7 @@
 #if defined(TWO_NODES_TX_RX)
 
 #define def_Neighbor2beFollowed
-#define OMAC_DEBUG_GPIO
+//#define OMAC_DEBUG_GPIO
 /* // Nathan's nodes
 //#define TXNODEID 18134
 //#define RXNODEID 20181
