606ed1f100581accca6c826bccf9a45c16202504
Bora Karaoglu <bora.karaoglu@samraksh.com>
2016-09-15 22:24:46 -0400

    Increase GB for debug purposes

--- MicroFrameworkPK_v4_3/DeviceCode/Include/Samraksh/MAC/OMAC/OMACConstants.h
+++ MicroFrameworkPK_v4_3/DeviceCode/Include/Samraksh/MAC/OMAC/OMACConstants.h
@@ -272,7 +272,7 @@ typedef OFProv<UINT64> OMACTicks;
 //GUARDTIME_MICRO should be calculated in conjuction with SLOT_PERIOD_MILLI
 // GUARDTIME_MICRO = (SLOT_PERIOD_MILLI - PacketTime)/2 - SWITCHING_DELAY_MICRO
 //PacketTime = 125byte * 8 bits/byte / (250*10^3 bits/sec) = 4sec
-#define GUARDTIME_MICRO 1500
+#define GUARDTIME_MICRO 15000
 #define MILLISECINMICSEC 1000
 
 
