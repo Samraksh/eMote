0fedf328e785d2cb2cdb10b0d7519606985d8b65
Bora Karaoglu <bora.karaoglu@samraksh.com>
2016-09-15 21:08:20 -0400

    DIsable StatePIN

--- MicroFrameworkPK_v4_3/Solutions/EmoteDotNow/platform_selector.h
+++ MicroFrameworkPK_v4_3/Solutions/EmoteDotNow/platform_selector.h
@@ -421,7 +421,7 @@ J12_PIN10 = GND
 #define OMAC_RX_DATAACK_PIN 				(GPIO_PIN)120 //120 // 120 //23  //120							//J11_pin3 0
 
 //Radio Control
-#define RADIOCONTROL_STATEPIN 				(GPIO_PIN)23 //120 //23 //120 // 120 //120 				//J11_pin6 //This 	(GPIO_PIN)3  did not work
+#define RADIOCONTROL_STATEPIN 				(GPIO_PIN)120 //120 //23 //120 // 120 //120 				//J11_pin6 //This 	(GPIO_PIN)3  did not work
 
 //TX Related
 #define RADIOCONTROL_SEND_PIN 				(GPIO_PIN)120 				//J11_pin4
