3fc76e49b3dbd7f91ea00b42cece0d90f6c7e9a5
ChrisAtSamraksh <chris.morse@samraksh.com>
2017-01-18 12:05:20 -0500

    Release 41 - 12 max neighbors to be used for relays and base.

--- MicroFrameworkPK_v4_3/DeviceCode/Include/Samraksh/Neighbors.h
+++ MicroFrameworkPK_v4_3/DeviceCode/Include/Samraksh/Neighbors.h
@@ -21,7 +21,7 @@
 #define TimeSync_Send_Buffer_15_4_t_SIZE 	1
 
 extern UINT8 MacName;
-#define MAX_NEIGHBORS 5
+#define MAX_NEIGHBORS 12
 #define INVALID_NEIGHBOR_INDEX 255
 #define INVALID_MACADDRESS 0
 #define INVALID_MACADDRESS2 65535
