8e1565ad795dc31e89ad16b7ce6d6e130f19fc72
MukundanAtSamraksh <mukundan.sridharan@samraksh.com>
2017-03-01 23:59:00 -0500

    Adding initial crypto token files

--- MicroFrameworkPK_v4_3/Solutions/SmartFusion2/DeviceCode/Crypto/PKCS11_Config_SF2_net.cpp
+++ MicroFrameworkPK_v4_3/Solutions/SmartFusion2/DeviceCode/Crypto/PKCS11_Config_SF2_net.cpp
@@ -1,14 +1,14 @@
 #include <PAL\PKCS11\CryptokiPAL.h>
 
-extern CK_SLOT_INFO  g_OpenSSL_SlotInfo;
-extern CryptokiToken g_OpenSSL_Token;
+extern CK_SLOT_INFO  g_AES_Hardware_SlotInfo;
+extern CryptokiToken g_AES_Hardware_Token;
 
 
 CryptokiSlot  g_CryptokiSlots[] = 
 {
     {
-        &g_OpenSSL_SlotInfo,
-        &g_OpenSSL_Token,
+        &g_AES_Hardware_SlotInfo,
+        &g_AES_Hardware_Token,
     },
 };
 
