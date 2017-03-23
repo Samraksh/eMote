a3ad6b7d60dddcf78bce2748eade4097e9de18ee
ChrisAtSamraksh <chris.morse@samraksh.com>
2017-03-23 14:01:35 -0400

    RTC timer fix.

--- MicroFrameworkPK_v4_3/DeviceCode/Targets/Native/STM32F10x/DeviceCode/drivers/Timer/netmf_rtc/netmf_rtc.cpp
+++ MicroFrameworkPK_v4_3/DeviceCode/Targets/Native/STM32F10x/DeviceCode/drivers/Timer/netmf_rtc/netmf_rtc.cpp
@@ -24,7 +24,26 @@ UINT32 STM32F10x_RTC::GetCounter()
 
 UINT64 STM32F10x_RTC::Get64Counter()
 {
+	GLOBAL_LOCK(irq);
 	UINT32 currentValue = RTC_GetCounter();
+
+	if(RTC_GetFlagStatus(RTC_FLAG_OW))
+	{
+		RTC_ClearFlag(RTC_FLAG_OW);
+
+		// An overflow just happened, updating variable that holds system time
+		g_STM32F10x_RTC.m_systemTime += (0x1ull <<32);
+		RTC_WaitForLastTask();
+		RTC_WaitForSynchro();
+		currentValue = RTC_GetCounter();
+	}
+
+	m_systemTime &= (0xFFFFFFFF00000000ull);
+	m_systemTime |= currentValue;
+
+	/*UINT64 firstReading = m_systemTime;
+
+	currentValue = RTC_GetCounter();
 
 	if(RTC_GetFlagStatus(RTC_FLAG_OW))
 	{
@@ -37,6 +56,16 @@ UINT64 STM32F10x_RTC::Get64Counter()
 	m_systemTime &= (0xFFFFFFFF00000000ull);
 	m_systemTime |= currentValue;
 
+	if ((m_systemTime > (firstReading + 3))|| (m_systemTime < firstReading)){
+		//hal_printf("\r\nrtc first: %llu cur: %llu\r\n",firstReading,m_systemTime);
+		currentValue = RTC_GetCounter();
+
+		m_systemTime &= (0xFFFFFFFF00000000ull);
+		m_systemTime |= currentValue;
+	return m_systemTime;
+	}
+	else */
+		
 	return m_systemTime;
 }
 
