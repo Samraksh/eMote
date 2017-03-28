76a98b4c7b2656d541fab563c3bf670dc6cde69d
Mukundan Sridharan <mukundan.sridharan@samraksh.com>
2017-03-28 18:17:56 -0400

    incremental commit

--- MicroFrameworkPK_v4_3/DeviceCode/pal/Samraksh/Crypto/AES_PKCS/SW_Tokens/AES_SW_PKCS11_objects.cpp
+++ MicroFrameworkPK_v4_3/DeviceCode/pal/Samraksh/Crypto/AES_PKCS/SW_Tokens/AES_SW_PKCS11_objects.cpp
@@ -2,7 +2,65 @@
 
 CK_RV AES_SW_PKCS11_Objects::CreateObject(Cryptoki_Session_Context* pSessionCtx, CK_ATTRIBUTE_PTR pTemplate, CK_ULONG ulCount, CK_OBJECT_HANDLE_PTR phObject)
 {
-    return CKR_FUNCTION_NOT_SUPPORTED;
+	UINT32 attribIndex = 0;
+
+	    if(pTemplate[attribIndex].type == CKA_CLASS && pTemplate[attribIndex].ulValueLen == sizeof(CK_OBJECT_CLASS))
+	    {
+	        CK_OBJECT_CLASS cls = SwapEndianIfBEc32(*(CK_OBJECT_CLASS*)pTemplate[attribIndex].pValue);
+	        CHAR pwd[64];
+	        int len;
+
+	        switch(cls)
+	        {
+	            case CKO_CERTIFICATE:
+	                for(++attribIndex; attribIndex < ulCount; attribIndex++)
+	                {
+	                    switch(pTemplate[attribIndex].type)
+	                    {
+	                        case CKA_PASSWORD:
+	                            len = pTemplate[attribIndex].ulValueLen;
+
+	                            if(len >= ARRAYSIZE(pwd))
+	                            {
+	                                len = ARRAYSIZE(pwd) - 1;
+	                            }
+
+	                            memcpy(pwd, pTemplate[attribIndex].pValue, len);
+
+	                            pwd[len] = 0;
+
+	                            break;
+
+	                        case CKA_VALUE:
+	                        {
+	                            OBJECT_DATA* pObject = NULL;
+	                            EVP_PKEY* privateKey = NULL;
+	                            X509 *x =  ssl_parse_certificate(pTemplate[attribIndex].pValue, pTemplate[attribIndex].ulValueLen, pwd, &privateKey);
+
+	                            if (x == NULL)
+	                            {
+	                                TINYCLR_SSL_PRINTF("Unable to load certificate\n");
+	                                return CKR_FUNCTION_FAILED;
+	                            }
+	                            else
+	                            {
+	                                CK_RV retVal = LoadX509Cert(pSessionCtx, x, &pObject, privateKey, phObject);
+
+	                                if(retVal != CKR_OK) X509_free(x);
+
+	                                return retVal;
+	                            }
+
+	                        }
+	                    }
+	                }
+	                break;
+
+	        }
+	    }
+
+	    return CKR_FUNCTION_NOT_SUPPORTED;
+
 }
 
 CK_RV AES_SW_PKCS11_Objects::CopyObject(Cryptoki_Session_Context* pSessionCtx, CK_OBJECT_HANDLE hObject, CK_ATTRIBUTE_PTR pTemplate, CK_ULONG ulCount, CK_OBJECT_HANDLE_PTR phNewObject)
