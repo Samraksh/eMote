46053f39918e8800fb37cb97f5c80a60baf732dd
Michael McGrath <Michael.McGrath@samraksh.com>
2017-04-13 18:03:20 -0400

    Update PAK_decl.h

--- MicroFrameworkPK_v4_3/DeviceCode/Include/PAK_decl.h
+++ MicroFrameworkPK_v4_3/DeviceCode/Include/PAK_decl.h
@@ -96,6 +96,7 @@ private:
 
 public:
     static const INT32 COMM_BUFFER_SIZE = IEEE802_15_4_MAX_PAYLOAD;
+    static const INT32 MAX_MSG_PAYLOAD = COMM_BUFFER_SIZE - (sizeof(simple_payload_t) - 1);
 
     MACEventHandler PAK_EventHandler;  //!< handle incoming wireless events.
     UINT8 PAK_MacName;                 //!< really enum MacName, aka MacId in other parts of the API.
