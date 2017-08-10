3906a580d4100b1a1b244ea4d94353ba5944c2bd
Bora Karaoglu <bora.karaoglu@samraksh.com>
2017-08-10 11:45:59 -0400

    Dropping dependency of the PAK

--- MicroFrameworkPK_v4_3/DeviceCode/pal/Samraksh/PAK/PortableArrayKitMessage.cpp
+++ MicroFrameworkPK_v4_3/DeviceCode/pal/Samraksh/PAK/PortableArrayKitMessage.cpp
@@ -39,7 +39,8 @@
 #include <Samraksh\Mac_decl.h>
 #include <Samraksh\MAC.h>
 // RF231.h is needed for enum RadioID and enum RadioStateEnum.  enums should be pulled into a public API header file.
-#include "..\..\..\Targets\Native\STM32F10x\DeviceCode\drivers\radio\RF231\RF231.h"
+//#include "..\..\..\Targets\Native\STM32F10x\DeviceCode\drivers\radio\RF231\RF231.h"
+#include <gpio\netmf_gpio.h>
 
 extern NeighborTable g_NeighborTable;
 //////////////////////////////////////////////////////////////////////////////
