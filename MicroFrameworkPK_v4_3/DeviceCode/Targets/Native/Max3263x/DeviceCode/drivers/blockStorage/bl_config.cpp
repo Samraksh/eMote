80d15f37edc4ad5559072698e712f5f7ca5f3f84
ChrisAtSamraksh <chris.morse@samraksh.com>
2017-08-23 13:10:59 -0400

    Memory map update for Max3263x.

--- MicroFrameworkPK_v4_3/DeviceCode/Targets/Native/Max3263x/DeviceCode/drivers/blockStorage/bl_config.cpp
+++ MicroFrameworkPK_v4_3/DeviceCode/Targets/Native/Max3263x/DeviceCode/drivers/blockStorage/bl_config.cpp
@@ -17,16 +17,16 @@ const BlockRange Max3263x_BlockRange_InternalFlash[] =
         // It should be obvious that block ranges are inclusive, and different regions have exclusive ranges.
         // TODO: check if User Storage B region is needed for EWR Extended Weak References to work (eg, for wear-leveling).
 
-	    { BlockRange::BLOCKTYPE_BOOTSTRAP       ,  0, 63 },  // 64 blocks, 128K for Booter,     address 0x0800_0000
-	    { BlockRange::BLOCKTYPE_CODE            , 64,385 },  //322 blocks, 644K for CLR,        address 0x0802_0000
-	    { BlockRange::BLOCKTYPE_DEPLOYMENT      ,386,449 },  // 64 blocks, 128K for Deployment, address 0x080C_1000
+	    { BlockRange::BLOCKTYPE_BOOTSTRAP       ,  0, 31 },  // 32 blocks, 256K for Booter,     address 0x0000_0000
+	    { BlockRange::BLOCKTYPE_CODE            , 32,192 },  //161 blocks, 1288K for CLR,        address 0x0004 0000
+	    { BlockRange::BLOCKTYPE_DEPLOYMENT      ,193,224 },  // 32 blocks, 256K for Deployment, address 0x0018 2000
 #if defined(SAMRAKSH_UPDATE_EXT )
-	    { BlockRange::BLOCKTYPE_UPDATE          ,450,505 },  // 56 blocks, 112K for Updates,    address 0x080E_1000
-	    { BlockRange::BLOCKTYPE_STORAGE_A       ,506,509 },  //  4 blocks,   8K for Storage A,  address 0x080F_9000
+	    { BlockRange::BLOCKTYPE_UPDATE          ,225,251 },  // 27 blocks, 216K for Updates,    address 0x001C 2000
+	    { BlockRange::BLOCKTYPE_STORAGE_A       ,252,253 },  //  2 blocks,   16K for Storage A,  address 0x001F 8000
 #else
-	    { BlockRange::BLOCKTYPE_STORAGE_A       ,450,509 },  // 60 blocks, 120K for Storage A,  address 0x080E_1000
+	    { BlockRange::BLOCKTYPE_STORAGE_A       ,225,253 },  // 60 blocks, 120K for Storage A,  address 0x001F 8000
 #endif
-	    { BlockRange::BLOCKTYPE_CONFIG          ,510,511 }   //  2 blocks,   4K for Config,     address 0x080F_F000
+	    { BlockRange::BLOCKTYPE_CONFIG          ,254,255 }   //  2 blocks,   16K for Config,     address 0x001F C000
 
 
 };
@@ -38,9 +38,9 @@ const BlockRange Max3263x_BlockRange_InternalFlash[] =
 const BlockRegionInfo  Max3263x_BlockRegionInfo_InternalFlash[1] =
 {
 
-    0x08000000,		// ByteAddress     Address;            // Start address
-    512,			// UINT32          NumBlocks;          // total number of blocks in this region
-    0x800,			// UINT32          BytesPerBlock;      // Total number of bytes per block (MUST be SectorsPerBlock * DataBytesPerSector)
+    0x00000000,		// ByteAddress     Address;            // Start address
+    256,			// UINT32          NumBlocks;          // total number of blocks in this region
+    0x2000,			// UINT32          BytesPerBlock;      // Total number of bytes per block (MUST be SectorsPerBlock * DataBytesPerSector)
 
     ARRAYSIZE_CONST_EXPR(Max3263x_BlockRange_InternalFlash),
     Max3263x_BlockRange_InternalFlash,
@@ -63,25 +63,6 @@ BlockDeviceInfo Max3263x_BlockDeviceInfo_InternalFlash =
     Max3263x_BlockRegionInfo_InternalFlash,	// pointer to an array (NumRegions long) of region information
 };
 
-/*
-BlockDeviceInfo Max3263x_BlockDeviceInfo_InternalFlash =
-{
-    {
-        FALSE,									// BOOL Removable;
-        TRUE,									// BOOL SupportsXIP;
-        TRUE,									// BOOL WriteProtected
-    },
-
-    9600,										// UINT32 Duration_Max_WordWrite_uSec; @todo
-    4800,										// UINT32 Duration_Max_SectorErase_uSec; @todo
-    0x4,										// BytesPerSector;
-
-    0x80000,									// UINT32 Size;
-
-    1,											// UINT32 NumRegions;
-    Max3263x_BlockRegionInfo_InternalFlash,	// const BlockRegionInfo* pRegions;
-};
-*/
 
 BLOCK_CONFIG Max3263x_blConfig_InternalFlash =
 {
