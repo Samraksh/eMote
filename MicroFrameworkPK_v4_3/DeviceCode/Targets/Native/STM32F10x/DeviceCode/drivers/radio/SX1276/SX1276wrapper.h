8d55a1e24ae75bf78df6231620f73da60b40bf78
Bora Karaoglu <bora.karaoglu@samraksh.com>
2017-08-31 16:40:31 -0400

    Removing virtual keywords

--- MicroFrameworkPK_v4_3/DeviceCode/Targets/Native/STM32F10x/DeviceCode/drivers/radio/SX1276/SX1276wrapper.h
+++ MicroFrameworkPK_v4_3/DeviceCode/Targets/Native/STM32F10x/DeviceCode/drivers/radio/SX1276/SX1276wrapper.h
@@ -35,9 +35,9 @@ public:
 	SX1276M1BxASWrapper();
 	void Initialize(SX1276_Semtech::RadioEvents_t *events);
 //	SX1276M1BxASWrapper(RadioEvents_t *events );
-	virtual ~SX1276M1BxASWrapper();
+	~SX1276M1BxASWrapper();
 
-	virtual void SetOpMode_public( uint8_t opMode );
+	void SetOpMode_public( uint8_t opMode );
 
     /*!
      * @brief Checks if the given RF frequency is supported by the hardware
@@ -45,21 +45,21 @@ public:
      * @param [IN] frequency RF frequency to be checked
      * @retval isSupported [true: supported, false: unsupported]
      */
-	virtual bool CheckRfFrequency( uint32_t frequency );
+	bool CheckRfFrequency( uint32_t frequency );
     /*!
      * @brief Writes the radio register at the specified address
      *
      * @param [IN]: addr Register address
      * @param [IN]: data New register value
      */
-    virtual void Write ( uint8_t addr, uint8_t data );
+    void Write ( uint8_t addr, uint8_t data );
     /*!
      * @brief Reads the radio register at the specified address
      *
      * @param [IN]: addr Register address
      * @retval data Register value
      */
-    virtual uint8_t Read ( uint8_t addr );
+    uint8_t Read ( uint8_t addr );
     /*!
      * @brief Writes multiple radio registers starting at address
      *
@@ -67,7 +67,7 @@ public:
      * @param [IN] buffer Buffer containing the new register's values
      * @param [IN] size   Number of registers to be written
      */
-    virtual void Write( uint8_t addr, uint8_t *buffer, uint8_t size );
+    void Write( uint8_t addr, uint8_t *buffer, uint8_t size );
     /*!
      * @brief Reads multiple radio registers starting at address
      *
@@ -75,25 +75,25 @@ public:
      * @param [OUT] buffer Buffer where to copy the registers data
      * @param [IN] size Number of registers to be read
      */
-    virtual void Read ( uint8_t addr, uint8_t *buffer, uint8_t size );
+    void Read ( uint8_t addr, uint8_t *buffer, uint8_t size );
     /*!
      * @brief Writes the buffer contents to the SX1276 FIFO
      *
      * @param [IN] buffer Buffer containing data to be put on the FIFO.
      * @param [IN] size Number of bytes to be written to the FIFO
      */
-    virtual void WriteFifo( uint8_t *buffer, uint8_t size );
+    void WriteFifo( uint8_t *buffer, uint8_t size );
     /*!
      * @brief Reads the contents of the SX1276 FIFO
      *
      * @param [OUT] buffer Buffer where to copy the FIFO read data.
      * @param [IN] size Number of bytes to be read from the FIFO
      */
-    virtual void ReadFifo( uint8_t *buffer, uint8_t size );
+    void ReadFifo( uint8_t *buffer, uint8_t size );
     /*!
      * @brief Resets the SX1276
      */
-    virtual void Reset(  );
+    void Reset(  );
 
     friend class Samraksh_SX1276_hal;
 
@@ -101,40 +101,40 @@ protected:
     /*!
      * @brief Initializes the radio I/Os pins interface
      */
-    virtual void IoInit( );
+    void IoInit( );
 
-    virtual void InitializeTimers();
+    void InitializeTimers();
 
     /*!
      *    @brief Initializes the radio registers
      */
-    virtual void RadioRegistersInit( );
+    void RadioRegistersInit( );
 
     /*!
      * @brief Initializes the radio SPI
      */
-    virtual void SpiInit(  );
+    void SpiInit(  );
 
     /*!
      * @brief Initializes DIO IRQ handlers
      *
      * @param [IN] irqHandlers Array containing the IRQ callback functions
      */
-    virtual void IoIrqInit( );
+    void IoIrqInit( );
 
     /*!
      * @brief De-initializes the radio I/Os pins interface.
      *
      * \remark Useful when going in MCU lowpower modes
      */
-    virtual void IoDeInit(  );
+    void IoDeInit(  );
 
     /*!
      * @brief Sets the radio output power.
      *
      * @param [IN] power Sets the RF output power
      */
-    virtual void SetRfTxPower( int8_t power );
+    void SetRfTxPower( int8_t power );
 
     /*!
      * @brief Gets the board PA selection configuration
@@ -142,26 +142,26 @@ protected:
      * @param [IN] channel Channel frequency in Hz
      * @retval PaSelect RegPaConfig PaSelect value
      */
-    virtual uint8_t GetPaSelect( uint32_t channel );
+    uint8_t GetPaSelect( uint32_t channel );
 
     /*!
      * @brief Set the RF Switch I/Os pins in Low Power mode
      *
      * @param [IN] status enable or disable
      */
-    virtual void SetAntSwLowPower( bool status );
+    void SetAntSwLowPower( bool status );
 
     /*!
      * @brief Initializes the RF Switch I/Os pins interface
      */
-    virtual void AntSwInit(  );
+    void AntSwInit(  );
 
     /*!
      * @brief De-initializes the RF Switch I/Os pins interface
      *
      * \remark Needed to decrease the power consumption in MCU lowpower modes
      */
-    virtual void AntSwDeInit(  );
+    void AntSwDeInit(  );
 
     /*!
      * @brief Controls the antenna switch if necessary.
@@ -170,15 +170,15 @@ protected:
      *
      * @param [IN] opMode Current radio operating mode
      */
-    virtual void SetAntSw( uint8_t opMode );
+    void SetAntSw( uint8_t opMode );
 
 
-    virtual void SetTimeoutTimer(TimeoutName_t ton, float delay) ;
-    virtual void CancelTimeoutTimer(TimeoutName_t ton);
+    void SetTimeoutTimer(TimeoutName_t ton, float delay) ;
+    void CancelTimeoutTimer(TimeoutName_t ton);
 
 
 private:
-    virtual uint8_t GetTimerID(TimeoutName_t ton) ;
+    uint8_t GetTimerID(TimeoutName_t ton) ;
 
 };
 
