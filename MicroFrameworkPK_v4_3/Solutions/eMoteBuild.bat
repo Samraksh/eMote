20dec835f150434dd38547f6ba1df8d70b19609b
Mukundan Sridharan <mukundan.sridharan@samraksh.com>
2018-03-15 16:58:48 -0400

    Builds for gcc 6.3.1, adding powershell build scripts

--- MicroFrameworkPK_v4_3/Solutions/eMoteBuild.bat
+++ MicroFrameworkPK_v4_3/Solutions/eMoteBuild.bat
@@ -3,6 +3,9 @@
 TITLE Emote Dev Command Prompt
 
 ECHO Welcome to Samraksh Emote Platform
+ECHO Usage: eMoteBuild.bat Solution Project (build or clean)
+ECHO Example: eMoteBuild.bat EmoteDotNow TinyCLR build
+
 set cur_path=%cd%
 set repo_path=%cur_path%\..\..
 echo Repo path is %repo_path%
@@ -10,9 +13,10 @@ echo Repo path is %repo_path%
 set env_cmd=%cur_path%\..\setenv_gcc.cmd
 echo Envscript cmd is %env_cmd%
 
-CMD /K "%env_cmd% 5.4.1 %repo_path%\..\TestSystem\Compilers\GCC5.4.1\ && cd %1 && cd %2 && msbuild %2.proj /t:%3 && echo All done && cd ..\.. "
-
 :: CMD /K "%env_cmd% 5.4.1 %repo_path%\..\TestSystem\Compilers\GCC5.4.1\ && cd %1 && cd %2 && msbuild %2.proj /t:%3 /maxcpucount && echo All done && cd ..\.. "
 
+:: CMD /K "%env_cmd% 5.4.1 %repo_path%\..\TestSystem\Compilers\GCC5.4.1\ && cd %1 && cd %2 && msbuild %2.proj /t:%3 && echo All done && cd ..\.. "
+
+CMD /K "%env_cmd% 6.3.1 %repo_path%\..\TestSystem\Compilers\GCC6.3.1\ && cd %1 && cd %2 && msbuild %2.proj /t:%3 && echo All done && cd ..\.. "
 
  
