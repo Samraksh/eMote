92be34cc6d3d2a6d4ab8f81cc8cfce97a575cdea
Mukundan Sridharan <Mukundan.Sridharan@Samraksh.com>
2019-02-01 15:00:50 -0500

    Minor changes

--- MicroFrameworkPK_v4_3/Solutions/EmoteDotNow/platform_selector.h
+++ MicroFrameworkPK_v4_3/Solutions/EmoteDotNow/platform_selector.h
@@ -26,8 +26,7 @@ typedef uint16_t ushort;
 // To enable, define SAMRAKSH_UPDATE_EXT, uncomment MFUpdate section of TinyCLR.proj, swap commented addresses in scatterfile_tinyclr_gcc.xml
 //#define SAMRAKSH_UPDATE_EXT
 
-
-// #define TINYCLR_SOLO      // change some base addresses when no TinyBooter on device.
+#define TINYCLR_SOLO      // change some base addresses when no TinyBooter on device.
 
 
 
